fd110418afc6f561bc94ca103bdc247b358db57a
Daniel Velkov <danielvelkov13@gmail.com>
2025-08-12 14:59:38 +0300

    feat(auth): create jwt strategy

---
+++ libs/util-auth/src/lib/strategies/jwt-strategy.ts
@@ -0,0 +1,27 @@
+import {
+  Strategy as JwtStrategy,
+  ExtractJwt,
+  StrategyOptionsWithoutRequest,
+  VerifyCallback,
+} from 'passport-jwt';
+
+import { client } from '@dans-coding-world/user-data-access';
+
+const opts: StrategyOptionsWithoutRequest = {
+  jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
+  secretOrKey: process.env.SECRET_KEY?.toString() ?? '',
+};
+
+const verify: VerifyCallback = async (jwt_payload, done) => {
+  try {
+    const user = await client.getById(jwt_payload.sub); //'sub' stands for subject
+    if (!user) return done(null, false, { message: 'Invalid token' });
+
+    done(null, user);
+  } catch (error) {
+    console.error(error);
+    done(error);
+  }
+};
+
+export const strategy = new JwtStrategy(opts, verify);
