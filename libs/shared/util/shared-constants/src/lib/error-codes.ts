f7406148f7a8ce69731b1d2301eca116164cb74f
Daniel Velkov <danielvelkov13@gmail.com>
2025-08-29 11:50:00 +0300

    feat(util): add error code constants

---
+++ libs/shared/util/shared-constants/src/lib/error-codes.ts
@@ -0,0 +1,27 @@
+type ValueOf<T> = T[keyof T];
+
+/**
+ * Centralized error codes for the app.
+ * Format: [MODULE][ERROR]-[CODE]
+ */
+export const ERROR_CODES = {
+  AUTH: {
+    INVALID_CREDENTIALS: 'AUTH001',
+  },
+} as const;
+
+type ErrorCode = ValueOf<ValueOf<typeof ERROR_CODES>>;
+
+/**
+ * Centralized default error messages for the app.
+ */
+export const ERROR_MESSAGES: Record<ErrorCode, string> = {
+  AUTH001: 'Provided credentials are invalid',
+};
+
+/**
+ * Centralized http status codes for errors
+ */
+export const ERROR_HTTP_STATUS: Record<ErrorCode, number> = {
+  AUTH001: 401,
+};
