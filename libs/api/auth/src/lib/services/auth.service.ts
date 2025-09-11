1d0f7882a408fc06816c68af558414b57938a55f
Daniel Velkov <danielvelkov13@gmail.com>
2025-09-11 15:23:15 +0300

    refactor(auth-service): streamline token generation and user repository usage

--- libs/api/auth/src/lib/services/auth.service.ts
+++ libs/api/auth/src/lib/services/auth.service.ts
@@ -1,21 +1,21 @@
 import { IAuthService } from '../interfaces/auth-service.interface.js';
 import { ITokenService } from '../interfaces/token-service.interface.js';
+import { IUserRepository } from '@dans-coding-world/shared-data-access-interfaces';
 import { userRepo } from '@dans-coding-world/user-data-access';
 import { LoginDto, LoginResponseDto } from '@dans-coding-world/shared-auth-dto';
 import { ERROR_CODES } from '@dans-coding-world/shared-constants';
 import { ApiException } from '@dans-coding-world/exceptions';
 import { validPassword } from '../helper/password.helper.js';
-import config, { AuthConfiguration } from '../config/auth.config.js';
-import tokenService from './token.service.js';
+import TokenService from './token.service.js';
 
-class AuthService implements IAuthService {
+export class AuthService implements IAuthService {
   constructor(
-    public jwtConfig: AuthConfiguration = config,
-    public tokenService: ITokenService = tokenService
+    public tokenService: ITokenService = TokenService,
+    public users: IUserRepository = userRepo
   ) {}
   async login(dto: LoginDto): Promise<LoginResponseDto> {
     const { email, password } = dto;
-    const user = await userRepo.get({ email });
+    const user = await this.users.get({ email });
 
     if (!user) throw new ApiException(ERROR_CODES.AUTH.INVALID_CREDENTIALS);
 
@@ -24,10 +24,8 @@ class AuthService implements IAuthService {
     if (isPasswordValid) {
       const payload = { sub: user.id };
 
-      const accessToken = tokenService.generateAccessToken(payload);
-      const refreshToken = await tokenService.generateRefreshToken(
-        user.id.toString()
-      );
+      const accessToken = this.tokenService.generateAccessToken(payload);
+      const refreshToken = this.tokenService.generateRefreshToken(user);
       return { accessToken, refreshToken, user };
     } else throw new ApiException(ERROR_CODES.AUTH.INVALID_PASSWORD);
   }
