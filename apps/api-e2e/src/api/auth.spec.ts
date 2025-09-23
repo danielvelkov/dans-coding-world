430cff52862b6374bff04f3d10eb507c53b34bc5
Daniel Velkov <danielvelkov13@gmail.com>
2025-09-23 14:21:41 +0300

    test(api): add e2e tests for refresh tokens

--- apps/api-e2e/src/api/auth.spec.ts
+++ apps/api-e2e/src/api/auth.spec.ts
@@ -1,15 +1,19 @@
-import { seedUsers } from '@dans-coding-world/testing-setup';
+import { seedUsers, seedRefreshTokens } from '@dans-coding-world/testing-setup';
 import { BaseResponse } from '@dans-coding-world/api-types';
 import { LoginResponseDto } from '@dans-coding-world/shared-auth-dto';
 import { login, renewAuthToken } from '../helper/authentication.js';
 import { createErrorResponse } from '../helper/error-response.js';
+import { RefreshToken, User } from '@dans-coding-world/prisma-schema';
+
+let users: User[];
+let tokens: RefreshToken[];
 
 describe('POST /api/v1/auth/login', () => {
   beforeAll(async () => {
-    await seedUsers();
+    users = await seedUsers();
   });
   it('should return an access token on valid credentials', async () => {
-    const res = await login('moderator123@gmail.com', 'moderator123');
+    const res = await login(users[0].email, users[0].password);
 
     expect(res.status).toBe(200);
     const { data } = res.data as BaseResponse;
@@ -30,7 +34,7 @@ describe('POST /api/v1/auth/login', () => {
 
   it('should return an error message on wrong password provided', async () => {
     await expect(
-      login('moderator123@gmail.com', 'onomatopoeia123')
+      login(users[0].email, 'onomatopoeia123')
     ).rejects.toMatchObject(
       createErrorResponse(401, 'Provided password is wrong')
     );
@@ -38,8 +42,34 @@ describe('POST /api/v1/auth/login', () => {
 });
 
 describe('POST api/v1/auth/refresh', () => {
+  let userWithExpiredToken: User,
+    userWithRevokedToken: User,
+    userWithValidToken: User;
+
+  beforeAll(async () => {
+    users = await seedUsers();
+    [userWithExpiredToken, userWithRevokedToken, userWithValidToken] = users;
+    tokens = await seedRefreshTokens([
+      {
+        expiresAt: new Date(Date.now() - 1000 * 60),
+        revoked: false,
+        userId: userWithExpiredToken.id,
+      },
+      {
+        expiresAt: new Date(Date.now() + 1000 * 60 * 60 * 24 * 30),
+        revoked: true,
+        userId: userWithRevokedToken.id,
+      },
+      {
+        expiresAt: new Date(Date.now() + 1000 * 60 * 60 * 24 * 30),
+        revoked: false,
+        userId: userWithValidToken.id,
+      },
+    ]);
+  });
+
   it('should return an access/refresh token pair on valid refresh token', async () => {
-    const loginRes = await login('moderator123@gmail.com', 'moderator123');
+    const loginRes = await login(users[0].email, users[0].password);
     const { data: loginData } = loginRes.data as BaseResponse;
     const { refreshToken } = loginData as LoginResponseDto;
 
@@ -59,13 +89,45 @@ describe('POST api/v1/auth/refresh', () => {
     expect(refreshData).toHaveProperty('user');
   });
 
-  // it('should return an error message on an expired token', async () => {
-  //   const loginRes = await login('moderator123@gmail.com', 'moderator123');
-  //   const { data: loginData } = loginRes.data as BaseResponse;
-  //   const { refreshToken } = loginData as LoginResponseDto;
+  it('should return an access/refresh token pair on valid refresh token (entry already in db)', async () => {
+    const validTokenObj = tokens.find(
+      (t) => t.userId === userWithValidToken.id
+    );
 
-  //   return await expect(renewAuthToken(refreshToken)).rejects.toMatchObject(
-  //     createErrorResponse(401, 'Invalid Token')
-  //   );
-  // });
+    if (!validTokenObj) throw new Error('Missing test user');
+    const refreshRes = await renewAuthToken(validTokenObj.token);
+
+    expect(refreshRes.status).toBe(200);
+    const { data: refreshData } = refreshRes.data as BaseResponse;
+    expect(refreshData).toHaveProperty(
+      'message',
+      'New access and refresh token issued'
+    );
+  });
+
+  it('should return an error message on an expired token', async () => {
+    const expiredTokenObj = tokens.find(
+      (t) => t.userId === userWithExpiredToken.id
+    );
+    if (!expiredTokenObj) throw new Error('Missing test user');
+
+    return await expect(
+      renewAuthToken(expiredTokenObj.token)
+    ).rejects.toMatchObject(
+      createErrorResponse(401, 'Invalid or expired token.')
+    );
+  });
+
+  it('should return an error message on a revoked token', async () => {
+    const revokedTokenObj = tokens.find(
+      (t) => t.userId === userWithRevokedToken.id
+    );
+    if (!revokedTokenObj) throw new Error('Missing test user');
+
+    return await expect(
+      renewAuthToken(revokedTokenObj.token)
+    ).rejects.toMatchObject(
+      createErrorResponse(401, 'Invalid or expired token.')
+    );
+  });
 });
