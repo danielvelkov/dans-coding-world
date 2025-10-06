a4292384dfd823e46daf396bc12c4890694611ee
Daniel Velkov <danielvelkov13@gmail.com>
2025-10-06 22:44:06 +0300

    feat(api): add guards to users controller revoke user tokens method

--- apps/api/src/controllers/users.controller.ts
+++ apps/api/src/controllers/users.controller.ts
@@ -1,15 +1,19 @@
-import { NextFunction, Request, Response } from 'express';
+import type { NextFunction, Request, Response } from 'express';
 import { StatusCodes } from 'http-status-codes';
-import { ITokenService } from '@dans-coding-world/api-auth';
+import {
+  Authorized,
+  ITokenService,
+  RequiredRole,
+} from '@dans-coding-world/api-auth';
 import { SUCCESS_MESSAGES } from '@dans-coding-world/shared-constants';
 
 export class UsersController {
-  constructor(private tokenService: ITokenService) {}
-  revokeUserTokens = async (
-    req: Request,
-    res: Response,
-    next: NextFunction
-  ) => {
+  constructor(private tokenService: ITokenService) {
+    this.revokeUserTokens = this.revokeUserTokens.bind(this);
+  }
+  @Authorized()
+  @RequiredRole('MOD', 'ADMIN')
+  async revokeUserTokens(req: Request, res: Response, next: NextFunction) {
     try {
       const { id } = req.params;
       const revokedTokens = await this.tokenService.revokeAllUserRefreshTokens(
@@ -23,5 +27,5 @@ export class UsersController {
     } catch (error) {
       return next(error);
     }
-  };
+  }
 }
