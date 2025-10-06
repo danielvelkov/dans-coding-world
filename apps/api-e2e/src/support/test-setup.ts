a5cf27a6045849e5a88ad2add278f23a9f6264f0
Daniel Velkov <danielvelkov13@gmail.com>
2025-10-06 21:55:04 +0300

    test(e2e): add cookie storage to global axios client in jest test-setup file

--- apps/api-e2e/src/support/test-setup.ts
+++ apps/api-e2e/src/support/test-setup.ts
@@ -1,5 +1,7 @@
 /* eslint-disable */
 import axios from 'axios';
+import { wrapper } from 'axios-cookiejar-support';
+import { CookieJar } from 'tough-cookie';
 
 if (process.env.NODE_ENV !== 'test') throw new Error('NODE_ENV not in "test"');
 
@@ -7,5 +9,15 @@ module.exports = async function () {
   // Configure axios for tests to use.
   const host = process.env.HOST ?? 'localhost';
   const port = process.env.PORT ?? '3000';
+
+  // Create cookie jar
+  const jar = new CookieJar();
+
+  // Wrap axios with cookie jar support
+  wrapper(axios);
+
+  // Configure defaults
   axios.defaults.baseURL = `http://${host}:${port}`;
+  axios.defaults.jar = jar;
+  axios.defaults.withCredentials = true;
 };
