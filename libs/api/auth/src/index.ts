22c074a2c667429f2468bc11bcde4c0a55f97d60
Daniel Velkov <danielvelkov13@gmail.com>
2025-10-06 21:35:09 +0300

    feat(auth): create @Authorized() guard decorator
    Will throw if authentication fails when using the jwt strategy defined in PassportJwtStrategy class

--- libs/api/auth/src/index.ts
+++ libs/api/auth/src/index.ts
@@ -8,4 +8,5 @@ export * from './lib/interfaces/auth-service.interface.js';
 export * from './lib/interfaces/registration-service.interface.js';
 export * from './lib/interfaces/token-service.interface.js';
 export * from './lib/helper/password.helper.js';
+export * from './lib/guards/authorized.guard.js';
 export * from './lib/strategies/jwt.strategy.js';
---
+++ libs/api/auth/src/lib/guards/authorized.guard.ts
@@ -0,0 +1,52 @@
+import { NextFunction, Request, Response } from 'express';
+import passport from 'passport';
+import { User } from '@dans-coding-world/prisma-schema';
+import {
+  JWT_STRATEGY_NAME,
+  PassportJwtStrategy,
+} from '../strategies/jwt.strategy.js';
+import { ApiException } from '@dans-coding-world/exceptions';
+import { ERROR_CODES } from '@dans-coding-world/shared-constants';
+
+/**
+ * @summary Authorization guard that requires access token validation using JWT strategy.
+ * @description Request population of the 'user' field. Works according to PassportJwtStrategy.
+ * @see {@link PassportJwtStrategy}
+ * @example
+ * ```typescript
+ * export class AuthController {
+ * ⠀@Authorized()
+ *   async seeProfile() {
+ *     // Only logged in users can see
+ *   }
+ */
+export function Authorized() {
+  return function (
+    target: unknown,
+    propertyKey: string,
+    descriptor: PropertyDescriptor
+  ) {
+    const originalMethod = descriptor.value;
+
+    descriptor.value = function (
+      req: Request,
+      res: Response,
+      next: NextFunction
+    ) {
+      passport.authenticate(
+        JWT_STRATEGY_NAME,
+        { session: false },
+        (error: Error, user: User) => {
+          if (error) return next(error);
+
+          if (!user)
+            return next(new ApiException(ERROR_CODES.AUTH.UNAUTHORIZED));
+
+          req.user = user;
+
+          return originalMethod.call(this, req, res, next);
+        }
+      )(req, res, next);
+    };
+  };
+}
