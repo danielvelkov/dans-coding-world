bdde9f87c78709fae93e76122c1c0b96e623274b
Daniel Velkov <danielvelkov13@gmail.com>
2025-10-08 15:47:33 +0300

    ci(actions): fix github actions
    Squashed commit of the following:
    
        * fix(actions): error regarding e2e tests on ci
    
        When running the github workflow an error occurred for the using of '__dirname'
        Changing the file ext of jest.config to cjs fixed it. (also had to add type 'module' to package.json
    
        * ci(actions): add prisma client generator command before running nx tests
    
        * ci(github): update github actions ci workflow to handle db creation and env variables

--- apps/api-e2e/src/support/global-setup.ts
+++ apps/api-e2e/src/support/global-setup.ts
@@ -4,13 +4,29 @@ import { waitForPortOpen } from '@nx/node/utils';
 var __TEARDOWN_MESSAGE__: string;
 
 module.exports = async function () {
-  // Start services that that the app needs to run (e.g. database, docker-compose, etc.).
-  console.log('\nSetting up...\n');
+  try {
+    // Start services that that the app needs to run (e.g. database, docker-compose, etc.).
+    console.log('\nSetting up...\n');
 
-  const host = process.env.HOST ?? 'localhost';
-  const port = process.env.PORT ? Number(process.env.PORT) : 3000;
-  await waitForPortOpen(port, { host });
+    console.log('\nGetting host up...\n');
+    const host = process.env.HOST ?? 'localhost';
+    console.log('\nGetting port up...\n');
+    const port = process.env.PORT ? Number(process.env.PORT) : 3000;
+    console.log('\nResult:', { host, port });
+    await waitForPortOpen(port, { host });
 
-  // Hint: Use `globalThis` to pass variables to global teardown.
-  globalThis.__TEARDOWN_MESSAGE__ = '\nTearing down...\n';
+    // Hint: Use `globalThis` to pass variables to global teardown.
+    globalThis.__TEARDOWN_MESSAGE__ = '\nTearing down...\n';
+  } catch (err) {
+    console.error('❌ Global setup failed:', err);
+
+    // If it's an AggregateError, log each error
+    if (err instanceof AggregateError) {
+      for (const e of err.errors) {
+        console.error('↪ Individual error:', e);
+      }
+    }
+
+    throw err; // rethrow so Jest knows setup failed
+  }
 };
