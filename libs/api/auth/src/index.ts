5f238e2f36d6e8960c998bd0a49fb8e5e411869a
Daniel Velkov <danielvelkov13@gmail.com>
2025-10-10 13:08:50 +0300

    feat(auth): add @AttachUser() guard for optional user authentication
    This is for endpoints where the content changes based on whether you are a guest or logged in.

--- libs/api/auth/src/index.ts
+++ libs/api/auth/src/index.ts
@@ -10,4 +10,5 @@ export * from './lib/interfaces/token-service.interface.js';
 export * from './lib/helper/password.helper.js';
 export * from './lib/guards/authorized.guard.js';
 export * from './lib/guards/required-role.guard.js';
+export * from './lib/guards/attach-user.guard.js';
 export * from './lib/strategies/jwt.strategy.js';
---
+++ libs/api/auth/src/lib/guards/attach-user.guard.ts
@@ -0,0 +1,50 @@
+import { NextFunction, Request, Response } from 'express';
+import passport from 'passport';
+import { User } from '@dans-coding-world/prisma-schema';
+import {
+  JWT_STRATEGY_NAME,
+  PassportJwtStrategy,
+} from '../strategies/jwt.strategy.js';
+
+/**
+ * @summary User guard populates user field if access token validation using JWT strategy succeeds.
+ * @description Request population of the 'user' field. Works according to PassportJwtStrategy.
+ * @see {@link PassportJwtStrategy}
+ * @example
+ * ```typescript
+ * export class AuthController {
+ * ⠀@AttachUser()
+ *   async seeProfile() {
+ *      if(req.user)
+ *        // logged in
+ *      else
+ *        // guest
+ *   }
+ */
+export function AttachUser() {
+  return function (
+    target: unknown,
+    propertyKey: string,
+    descriptor: PropertyDescriptor
+  ) {
+    const originalMethod = descriptor.value;
+
+    descriptor.value = function (
+      req: Request,
+      res: Response,
+      next: NextFunction
+    ) {
+      passport.authenticate(
+        JWT_STRATEGY_NAME,
+        { session: false },
+        (error: Error, user: User) => {
+          if (error) return next(error);
+
+          if (user) req.user = user;
+
+          return originalMethod.call(this, req, res, next);
+        }
+      )(req, res, next);
+    };
+  };
+}
