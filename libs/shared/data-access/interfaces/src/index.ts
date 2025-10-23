3ca36bd3e57fbbe4d14e8f00a3c54f779dc38d85
Daniel Velkov <danielvelkov13@gmail.com>
2025-10-23 20:51:42 +0300

    feat(post): add post comments repo interface

--- libs/shared/data-access/interfaces/src/index.ts
+++ libs/shared/data-access/interfaces/src/index.ts
@@ -1,3 +1,4 @@
 export * from './lib/user-repository.interface.js';
 export * from './lib/refresh-token-repository.interface.js';
 export * from './lib/post-repository.interface.js';
+export * from './lib/comment-repository.interface.js';
