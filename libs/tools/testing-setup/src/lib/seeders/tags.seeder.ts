d9cc48c550f1639dc405dccfa3664d5ff8927a7c
Daniel Velkov <danielvelkov13@gmail.com>
2025-11-14 17:25:17 +0200

    test: add tags seeder and json mock tag data

---
+++ libs/tools/testing-setup/src/lib/seeders/tags.seeder.ts
@@ -0,0 +1,78 @@
+import { client, Tag, Post } from '@dans-coding-world/prisma-schema';
+import tags from '../data/tags.json' with {type: "json"};
+import { SeedOptions } from './types/seed-options.js';
+
+/**
+ * @description ⚠️ **Test-only method.** This function is intended for development and testing purposes only.
+ * 
+ * **🚨 Do not use in production.** It will delete existing tag data.
+ * 
+ * @param customTags Tags to create. 
+ * @param options Seed options for whether to reset the 'Tag' table and/or use default tags data
+ * 
+ * *DEFAULT DATA*:
+ * - 30 generic tags
+ */
+export const seedTags = async (
+  customTags?: Tag[],
+  options: SeedOptions = { clearExisting: true, useDefaults: true }
+): Promise<Tag[]> => {
+  if (!(process.env.NODE_ENV === 'test' || process.env.NODE_ENV === 'test_e2e'))
+    throw new Error(
+      'Not in test environment. Check your test setup configuration.'
+    );
+  try {
+    const seeded: Tag[] = [];
+
+    if (options.clearExisting) {
+      await client.tag.deleteMany();
+      await client.$queryRaw`ALTER SEQUENCE "Tag_id_seq" RESTART WITH 1;`;
+    }
+
+    if (options.useDefaults) {
+      const defaultTags = await createAndReturnTagsWithId(tags);
+      seeded.push(...defaultTags);
+    }
+
+    if (customTags) {
+      const newTags = await createAndReturnTagsWithId(customTags);
+      seeded.push(...newTags);
+    }
+    return seeded;
+  } catch (e) {
+    console.error(e);
+    process.exit(1);
+  } finally {
+    await client.$disconnect();
+  }
+};
+
+const createAndReturnTagsWithId = async (tags: any[]) => {
+  if (!tags.length) return [];
+  const createdTags = await client.$transaction(
+    tags.map((tag) => client.tag.create({ data: tag }))
+  );
+  return createdTags.map((u) => ({ ...u }));
+};
+
+/**
+ * Helper: Attach existing tags to existing post
+ * @param postId 
+ * @param tagIds 
+ * @returns The updated post
+ */
+export const attachTagsToPost = async (postId: number, tagIds: number[]): Promise<Post> => {
+ return await client.post.update({
+  where: { id: postId },
+  data: {
+    tags: {
+      create: tagIds.map(id => ({ tag: {
+        connect: {
+          id
+        }
+      } })) 
+    }
+  },
+  include: { tags: { include: { tag: true } } }
+});
+}
