9d1994966fc1519c6571c16ee6e333b078359a86
Daniel Velkov <danielvelkov13@gmail.com>
2025-11-27 21:26:44 +0200

    feat(shared): add interface for data access of comment reports

--- libs/shared/data-access/interfaces/src/index.ts
+++ libs/shared/data-access/interfaces/src/index.ts
@@ -3,3 +3,4 @@ export * from './lib/refresh-token-repository.interface.js';
 export * from './lib/post-repository.interface.js';
 export * from './lib/comment-repository.interface.js';
 export * from './lib/tag-repository.interface.js';
+export * from './lib/report-repository.interface.js';
---
+++ libs/shared/data-access/interfaces/src/lib/report-repository.interface.ts
@@ -0,0 +1,18 @@
+/**
+ * Interface for managing reports
+ */
+export interface IReportRepository<Report, TWhereInput, TOrderBy> {
+  getById(id: number): Promise<Report | null>;
+  create(data: Omit<Report, 'id'>): Promise<Report>;
+  search(
+    where?: TWhereInput,
+    orderBy?: TOrderBy,
+    options?: {
+      skip?: number;
+      take?: number;
+    }
+  ): Promise<Report[]>;
+  delete(id: number): Promise<Report>;
+  deleteMany(where: TWhereInput): Promise<number>;
+  exists(where: TWhereInput): Promise<boolean>;
+}
