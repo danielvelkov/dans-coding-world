71cf57a254226f7a0efb4f03fe2f73e666071026
Daniel Velkov <danielvelkov13@gmail.com>
2025-11-30 20:49:32 +0200

    feat(reports): add pagination values and constraints for reports on comments

--- libs/shared/util/shared-constants/src/index.ts
+++ libs/shared/util/shared-constants/src/index.ts
@@ -1,10 +1,13 @@
 export * from './lib/error-codes.js';
-export * from './lib/constraints/user.constraints.js';
-export * from './lib/constraints/token.constraints.js';
 export * from './lib/validation-messages.js';
 export * from './lib/success-messages.js';
-export * from './lib/auth.constants.js';
+
+export * from './lib/constraints/user.constraints.js';
+export * from './lib/constraints/token.constraints.js';
 export * from './lib/constraints/post.constraints.js';
 export * from './lib/constraints/comment.constraints.js';
+export * from './lib/constraints/report.constraints.js';
 export * from './lib/constraints/tag.constraints.js';
+
 export * from './lib/pagination.constants.js';
+export * from './lib/auth.constants.js';
