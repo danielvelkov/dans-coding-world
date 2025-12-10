2dd22a5ad716d32781459b4b2f9d620ea3a14546
Daniel Velkov <danielvelkov13@gmail.com>
2025-12-10 12:58:43 +0200

    docs(swagger): add swagger jsdoc for endpoint GET user/:id

--- apps/api/src/routes/users.router.ts
+++ apps/api/src/routes/users.router.ts
@@ -23,6 +23,64 @@ export default usersRouter;
 
 /**
  * @openapi
+ * /users/{id}:
+ *   get:
+ *     tags: [Users]
+ *     summary: Get user by Id.
+ *     description: |
+ *       Get a specific user by its Id along with its profile details if he has his profile set up.
+ *       If the user requesting it is the owner of the account, Moderator or Admin - the "email" field is included in the response
+ *
+ *     parameters:
+ *       - in: path
+ *         name: id
+ *         schema:
+ *           type: integer
+ *         required: true
+ *         description: Numeric ID of the user
+ *     responses:
+ *       200:
+ *         description: User details retrieved successfully
+ *         content:
+ *           application/json:
+ *             schema:
+ *               $ref: '#/components/schemas/GetUserResponse'
+ *       400:
+ *         description: Bad Request - Invalid query params
+ *         content:
+ *           application/json:
+ *             schema:
+ *               $ref: '#/components/schemas/ErrorResponse'
+ *             example:
+ *               success: false
+ *               data: null
+ *               error:
+ *                 status: 400
+ *                 errorCode: VAL001
+ *                 message: One or more fields failed validation
+ *       404:
+ *         description: Not Found - User does not exist
+ *         content:
+ *           application/json:
+ *             schema:
+ *               $ref: '#/components/schemas/ErrorResponse'
+ *             example:
+ *               success: false
+ *               data: null
+ *               error:
+ *                 status: 404
+ *                 errorCode: SER002
+ *                 message: Resource not found
+ *       500:
+ *         description: Internal Server Error
+ *         content:
+ *           application/json:
+ *             schema:
+ *               $ref: '#/components/schemas/InternalServerError'
+ */
+
+/**
+ * @openapi
  * /users/{userId}/revoke-tokens:
  *   post:
  *     tags: [Users]
@@ -97,4 +155,44 @@ export default usersRouter;
  *           type: string
  *           description: User role. Can be either ADMIN, MOD, AUTHOR or USER
  *           example: USER
+ *
+ *     Profile:
+ *       type: object
+ *       properties:
+ *         id:
+ *           type: string
+ *           description: Profile ID
+ *           example: 1
+ *         firstName:
+ *           type: string
+ *           description: User first name
+ *           example: John
+ *         lastName:
+ *           type: string
+ *           description: User last name
+ *           example: Doe
+ *         bio:
+ *           type: string
+ *           description: User bio
+ *           example: Very mysterious person
+ *         avatarUrl:
+ *           type: string
+ *           description: Url to avatar picture
+ *           example: some.cloud.storage/images/id
+ *
+ *     GetUserResponse:
+ *       allOf:
+ *         - $ref: '#/components/schemas/SuccessResponse'
+ *         - type: object
+ *           properties:
+ *             data:
+ *               type: object
+ *               properties:
+ *                 user:
+ *                   allOf:
+ *                     - $ref: '#/components/schemas/User'
+ *                     - type: object
+ *                       properties:
+ *                         profile:
+ *                           $ref: '#/components/schemas/Profile'
  */
