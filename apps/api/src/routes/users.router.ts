4b8691f60725c2319a4f0c80578cb977e533d397
Daniel Velkov <danielvelkov13@gmail.com>
2025-12-11 15:08:03 +0200

    docs(swagger): add swagger jsdoc for /users/password endpoint

--- apps/api/src/routes/users.router.ts
+++ apps/api/src/routes/users.router.ts
@@ -88,6 +88,74 @@ export default usersRouter;
 
 /**
  * @openapi
+ * /users/password:
+ *   patch:
+ *     tags: [Users]
+ *     summary: Update user's password.
+ *     description: |
+ *       Update logged-in user's password if the provided old password matches the
+ *       current one and the new password is valid. Requires user to be logged in.
+ *
+ *       **Authentication:**
+ *       - If access_token is not valid in Set-Cookie header, return 401 UNAUTHORIZED error
+ *       - If access_token is valid, user can change his password.
+ *     requestBody:
+ *       content:
+ *         application/json:
+ *           schema:
+ *             $ref: '#/components/schemas/UpdatePasswordDto'
+ *         application/x-www-form-urlencoded:
+ *           schema:
+ *             $ref: '#/components/schemas/UpdatePasswordDto'
+ *     responses:
+ *       200:
+ *         description: Password updated successfully
+ *         content:
+ *           application/json:
+ *             schema:
+ *               $ref: '#/components/schemas/GetUserResponse'
+ *       400:
+ *         description: Bad Request - Invalid form body
+ *         content:
+ *           application/json:
+ *             schema:
+ *               $ref: '#/components/schemas/ErrorResponse'
+ *             example:
+ *               success: false
+ *               data: null
+ *               error:
+ *                 status: 400
+ *                 errorCode: VAL001
+ *                 message: One or more fields failed validation
+ *       401:
+ *         description: Unauthorized - Login first
+ *         content:
+ *           application/json:
+ *             schema:
+ *               $ref: '#/components/schemas/UnauthorizedError'
+ *       404:
+ *         description: Not Found - User does not exist
+ *         content:
+ *           application/json:
+ *             schema:
+ *               $ref: '#/components/schemas/ErrorResponse'
+ *             example:
+ *               success: false
+ *               data: null
+ *               error:
+ *                 status: 404
+ *                 errorCode: SER002
+ *                 message: Resource not found
+ *       500:
+ *         description: Internal Server Error
+ *         content:
+ *           application/json:
+ *             schema:
+ *               $ref: '#/components/schemas/InternalServerError'
+ */
+
+/**
+ * @openapi
  * /users:
  *   patch:
  *     tags: [Users]
@@ -362,5 +430,22 @@ export default usersRouter;
  *       example:
  *         firstName: Jon.
  *         lastName: Doe
+ *
+ *     UpdatePasswordDto:
+ *       type: object
+ *       properties:
+ *         oldPassword:
+ *           type: string
+ *           description: User's old password
+ *           minLength: 8
+ *           maxLength: 32
+ *         newPassword:
+ *           type: string
+ *           description: User's new password
+ *           minLength: 8
+ *           maxLength: 32
+ *       example:
+ *         oldPassword: badPassword1.
+ *         newPassword: dkjafdld_skfj!al
  *
  */
