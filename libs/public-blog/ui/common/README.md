28b58417d0985687cd8ef5939296ec4253105c7b
Daniel Velkov <danielvelkov13@gmail.com>
2025-12-24 21:52:43 +0200

    feat(nx): generate react lib for common ui cmpnts used in public-blog FE

---
+++ libs/public-blog/ui/common/README.md
@@ -0,0 +1,7 @@
+# @dans-coding-world/public-blog-ui-common
+
+This library was generated with [Nx](https://nx.dev).
+
+## Running unit tests
+
+Run `nx test @dans-coding-world/public-blog-ui-common` to execute the unit tests via [Vitest](https://vitest.dev/).
---
+++ libs/public-blog/ui/common/src/lib/pagination.tsx
@@ -0,0 +1,14 @@
+import styled from 'styled-components';
+
+const StyledNav = styled.nav`
+  color: pink;
+`;
+export function Pagination() {
+  return (
+    <StyledNav>
+      <h1>Welcome to Pagination!</h1>
+    </StyledNav>
+  );
+}
+
+export default Pagination;
