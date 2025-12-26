88dad85c24e194ddad655d7893b98ce3da8f5aee
Daniel Velkov <danielvelkov13@gmail.com>
2025-12-26 20:28:50 +0200

    refactor: change prop list to use children prop

--- libs/public-blog/features/blog-list/src/lib/components/post-list.tsx
+++ libs/public-blog/features/blog-list/src/lib/components/post-list.tsx
@@ -1,5 +1,4 @@
 import styled from 'styled-components';
-import { PostItemData } from '../types/post-item-data.types';
 import { PostItem } from './post-item';
 
 export const EMPTY_POSTS_MESSAGE = 'No posts found.';
@@ -8,20 +7,11 @@ const StyledUnorderedList = styled.ul`
   list-style-type: none;
 `;
 
-export function PostList({ posts }: { posts: PostItemData[] }) {
-  if (posts.length === 0) return <p>{EMPTY_POSTS_MESSAGE}</p>;
-  else
-    return (
-      <StyledUnorderedList>
-        {posts.map((p) => (
-          <PostItem
-            post={p}
-            key={p.id}
-            onAuthorClick={(id) => console.log(id)}
-            onTagClick={(tagName) => console.log(tagName)}
-            isLocked={false}
-          ></PostItem>
-        ))}
-      </StyledUnorderedList>
-    );
+export function PostList({
+  children,
+}: {
+  children: React.ReactElement<typeof PostItem>[];
+}) {
+  if (children.length === 0) return <p>{EMPTY_POSTS_MESSAGE}</p>;
+  else return <StyledUnorderedList>{children}</StyledUnorderedList>;
 }
--- libs/public-blog/features/blog-list/src/lib/tests/post-list.spec.tsx
+++ libs/public-blog/features/blog-list/src/lib/tests/post-list.spec.tsx
@@ -2,21 +2,36 @@ import { render, screen, within } from '@testing-library/react';
 import '@testing-library/jest-dom';
 import { PostList, EMPTY_POSTS_MESSAGE } from '../components/post-list';
 import { mockPosts } from './mocks/post-item-data.mock';
+import { PostItem } from '../components/post-item';
+
+const validProps = {
+  onAuthorClick: vi.fn(),
+  onTagClick: vi.fn(),
+  isLocked: false,
+};
+
+const validList = (
+  <PostList>
+    {mockPosts.map((p) => (
+      <PostItem key={p.id} {...validProps} post={p}></PostItem>
+    ))}
+  </PostList>
+);
 
 describe('PostList', () => {
   it('renders successfully', () => {
-    const { baseElement } = render(<PostList posts={[...mockPosts]} />);
+    const { baseElement } = render(validList);
     expect(baseElement).toBeTruthy();
   });
 
-  it('renders <ul> with the number of <li> equal to the passed posts', () => {
-    render(<PostList posts={[...mockPosts]} />);
+  it('renders <ul> with the number of <li> equal to the passed post items', () => {
+    render(validList);
     const ul = screen.getByRole('list');
     expect(within(ul).getAllByRole('listitem').length).toBe(mockPosts.length);
   });
 
   it('renders PostItem components with correct props', () => {
-    render(<PostList posts={[...mockPosts]} />);
+    render(validList);
     for (const post of mockPosts)
       expect(
         screen.getByRole('heading', { name: post.title })
@@ -24,7 +39,7 @@ describe('PostList', () => {
   });
 
   it('renders message instead of <ul> for empty posts array', () => {
-    render(<PostList posts={[]} />);
+    render(<PostList children={[]} />);
     expect(screen.queryByRole('list')).not.toBeInTheDocument();
     expect(screen.getByText(EMPTY_POSTS_MESSAGE)).toBeInTheDocument();
   });
