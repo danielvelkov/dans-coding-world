9fd320552b2fe7f4ece226ca9c0ae0c41b9a6341
Daniel Velkov <danielvelkov13@gmail.com>
2026-01-07 14:38:59 +0200

    chore(nx): cleanup unused routes generated by nx

--- apps/public-blog/src/app/app.tsx
+++ apps/public-blog/src/app/app.tsx
@@ -21,37 +21,22 @@ export function App() {
         <div role="navigation">
           <ul>
             <li>
-              <Link to="/">Home</Link>
-            </li>
-            <li>
-              <Link to="/page-2">Page 2</Link>
+              <Link to="/blog">Blog</Link>
             </li>
           </ul>
         </div>
-        <BlogList
-          onPostClick={() => {
-            console.log('Post link clicked');
-          }}
-          onAuthorClick={() => {
-            console.log('Author link clicked');
-          }}
-        ></BlogList>
         <Routes>
           <Route
-            path="/"
+            path="/blog"
             element={
-              <div>
-                This is the generated root route.{' '}
-                <Link to="/page-2">Click here for page 2.</Link>
-              </div>
-            }
-          />
-          <Route
-            path="/page-2"
-            element={
-              <div>
-                <Link to="/">Click here to go back to root page.</Link>
-              </div>
+              <BlogList
+                onPostClick={() => {
+                  console.log('Post link clicked');
+                }}
+                onAuthorClick={() => {
+                  console.log('Author link clicked');
+                }}
+              ></BlogList>
             }
           />
         </Routes>
