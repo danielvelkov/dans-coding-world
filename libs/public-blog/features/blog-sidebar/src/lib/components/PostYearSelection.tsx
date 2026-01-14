a2ca2c2b7851e2d1fa694d7d0e390c28c9fc9c8f
Daniel Velkov <danielvelkov13@gmail.com>
2026-01-14 21:05:37 +0200

    feat(blog): add post year selection component

---
+++ libs/public-blog/features/blog-sidebar/src/lib/components/PostYearSelection.tsx
@@ -0,0 +1,61 @@
+import { UseQueryResult } from '@tanstack/react-query';
+import { GetPostsMetadataResponse } from '@dans-coding-world/shared-post-dto';
+import styled from 'styled-components';
+import React from 'react';
+
+const StyledYearButton = styled.button<
+  React.ComponentPropsWithoutRef<'button'> & { $selected: boolean }
+>`
+  background: none;
+  border: none;
+  padding: 0;
+  margin-right: 1rem;
+  font: inherit;
+  color: ${({ $selected }) => ($selected ? 'red' : 'inherit')};
+  cursor: pointer;
+
+  text-decoration: ${({ $selected }) => ($selected ? 'underline' : 'none')};
+  text-underline-offset: 3px;
+
+  &:hover {
+    text-decoration: underline;
+  }
+
+  &:focus-visible {
+    outline: 2px solid red;
+    outline-offset: 2px;
+  }
+`;
+
+export function PostYearSelection({
+  yearsQuery,
+  selectedYear,
+  onYearToggle,
+}: {
+  yearsQuery: UseQueryResult<GetPostsMetadataResponse>;
+  selectedYear?: number;
+  onYearToggle: (year: string) => void;
+}) {
+  const { data, isLoading, isError } = yearsQuery;
+
+  if (isLoading || isError || !data || !data.years || data.years.length === 0) {
+    return null;
+  }
+
+  return (
+    <section aria-labelledby="year-filter-heading">
+      <h3 id="year-filter-heading">Select Posts by year</h3>
+
+      {data.years.map((year) => (
+        <StyledYearButton
+          key={year}
+          $selected={year === selectedYear}
+          aria-pressed={year === selectedYear ? 'true' : undefined}
+          onClick={() => onYearToggle(year.toString())}
+        >
+          {year}
+        </StyledYearButton>
+      ))}
+    </section>
+  );
+}
