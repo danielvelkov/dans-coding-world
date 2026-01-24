4c30deeca1f9417dad1c66a418f7cb7e4577981a
Daniel Velkov <danielvelkov13@gmail.com>
2026-01-24 15:23:13 +0200

    feat(blog): add custom hook for get & set of /blog page params

--- libs/public-blog/shared/hooks/src/index.ts
+++ libs/public-blog/shared/hooks/src/index.ts
@@ -3,3 +3,4 @@ export * from './lib/posts/useFetchTags';
 export * from './lib/posts/useFetchPostsMetadata';
 export * from './lib/util/useDebounce';
 export * from './lib/types/fetchPostsQueryParams';
+export * from './lib/posts/usePostsQueryParams';
--- libs/public-blog/shared/hooks/src/lib/posts/useFetchPosts.ts
+++ libs/public-blog/shared/hooks/src/lib/posts/useFetchPosts.ts
@@ -1,16 +1,12 @@
 import { useQuery } from '@tanstack/react-query';
-import {
-  GetPostsResponseDto,
-  GetPostsDto,
-} from '@dans-coding-world/shared-post-dto';
+import { GetPostsResponseDto } from '@dans-coding-world/shared-post-dto';
 import { API_ENDPOINTS } from '@dans-coding-world/shared-data-access-api';
 import { api } from '@dans-coding-world/public-blog-data-access-api';
 import { BaseResponse } from '@dans-coding-world/api-types';
 import { handleQueryResponse } from '../helper/handle-query-response';
+import { FetchPostsQueryParams } from '../types/fetchPostsQueryParams';
 
 const TEN_MINUTES_IN_MS = 10 * 60 * 1000;
-
-export type FetchPostsQueryParams = Omit<GetPostsDto, 'viewerId'>;
 
 export const useFetchPosts = (params?: FetchPostsQueryParams) => {
   const query = useQuery({
