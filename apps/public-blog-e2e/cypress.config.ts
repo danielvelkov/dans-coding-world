e3de63887a21b5f1b7a97850c827544353ae1727
Daniel Velkov <danielvelkov13@gmail.com>
2026-01-28 16:49:45 +0200

    test(blog): add cypress tasks for seeding db

--- apps/public-blog-e2e/cypress.config.ts
+++ apps/public-blog-e2e/cypress.config.ts
@@ -1,18 +1,59 @@
 import { nxE2EPreset } from '@nx/cypress/plugins/cypress-preset';
 import { defineConfig } from 'cypress';
+import axios from 'axios';
+
+const host = process.env.HOST ?? 'localhost';
+const port = process.env.PORT ?? '3000';
+axios.defaults.baseURL = `http://${host}:${port}`;
+
+const nxConfig = nxE2EPreset(__filename, {
+  cypressDir: 'src',
+  bundler: 'vite',
+  webServerCommands: {
+    default: 'npx nx run public-blog:dev',
+    production: 'npx nx run public-blog:preview',
+  },
+  ciWebServerCommand: 'npx nx run public-blog:preview',
+  ciBaseUrl: 'http://localhost:4200',
+});
 
 export default defineConfig({
   e2e: {
-    ...nxE2EPreset(__filename, {
-      cypressDir: 'src',
-      bundler: 'vite',
-      webServerCommands: {
-        default: 'npx nx run @dans-coding-world/public-blog:dev',
-        production: 'npx nx run @dans-coding-world/public-blog:preview',
-      },
-      ciWebServerCommand: 'npx nx run @dans-coding-world/public-blog:preview',
-      ciBaseUrl: 'http://localhost:4200',
-    }),
+    ...nxConfig,
     baseUrl: 'http://localhost:4200',
+    async setupNodeEvents(on, config) {
+      // IMPORTANT: Execute the Nx preset's node events first
+      if (nxConfig.setupNodeEvents) {
+        await nxConfig.setupNodeEvents(on, config);
+      }
+
+      // Cannot import it normally for some reason
+      const { API_ENDPOINTS } = await import(
+        '@dans-coding-world/shared-data-access-api'
+      );
+
+      on('task', {
+        async 'db:seed-users'(args = {}) {
+          const { users, options } = args;
+          const {
+            data: { data },
+          } = await axios.post(`${API_ENDPOINTS.TEST_DATA.USERS}`, users, {
+            params: options,
+          });
+          return data;
+        },
+        async 'db:seed-posts'(args = {}) {
+          const { posts, options } = args;
+          const {
+            data: { data },
+          } = await axios.post(`${API_ENDPOINTS.TEST_DATA.POSTS}`, posts, {
+            params: options,
+          });
+          return data;
+        },
+      });
+
+      return config;
+    },
   },
 });
