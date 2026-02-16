c951133273fb88be21946c06644be1c9453afbf9
Daniel Velkov <danielvelkov13@gmail.com>
2026-02-16 16:40:41 +0200

    test(e2e): add tests and fixtures for publication year selection

--- apps/public-blog-e2e/src/e2e/blog/filters.cy.ts
+++ apps/public-blog-e2e/src/e2e/blog/filters.cy.ts
@@ -56,8 +56,98 @@ describe('Blog - filters', () => {
     });
   });
 
-  // TODO:
-  // context.only('By published year selection section', () => {
-  //   it('shows posts published ')
-  // });
+  context('by publication year', () => {
+    it('clicking on a year button marks it as the only one selected', () => {
+      const postsYears = [
+        ...new Set(
+          seededPosts.map((p) => new Date(p.publishedAt as Date).getFullYear())
+        ),
+      ];
+      for (const selectedYear of postsYears) {
+        cy.selectPostPublicationYearFilter(selectedYear);
+        cy.contains('button', selectedYear).should(
+          'have.attr',
+          'aria-pressed',
+          'true'
+        );
+        Cypress._.without(postsYears, selectedYear).forEach(
+          (notSelectedYear) => {
+            cy.contains('button', notSelectedYear).should(
+              'not.have.attr',
+              'aria-pressed'
+            );
+          }
+        );
+      }
+    });
+
+    it('selecting an active year filter deselects it', () => {
+      const postsYears = [
+        ...new Set(
+          seededPosts.map((p) => new Date(p.publishedAt as Date).getFullYear())
+        ),
+      ];
+      const selectedYear = postsYears[Cypress._.random(postsYears.length - 1)];
+      cy.selectPostPublicationYearFilter(selectedYear);
+      cy.contains('button', selectedYear).should(
+        'have.attr',
+        'aria-pressed',
+        'true'
+      );
+      cy.selectPostPublicationYearFilter(selectedYear);
+      cy.contains('button', selectedYear).should(
+        'not.have.attr',
+        'aria-pressed'
+      );
+    });
+
+    it('applies filtering correctly when selecting post publication year', () => {
+      const uniquePublicationYears = new Set(
+        seededPosts.map((p) => new Date(p.publishedAt as Date).getFullYear())
+      );
+      uniquePublicationYears.forEach((year) => {
+        cy.selectPostPublicationYearFilter(year);
+        cy.get('[aria-label="blog posts"] article time').each(($timeElm) => {
+          expect($timeElm.attr('datetime')).to.satisfy((timeString: string) =>
+            timeString.startsWith(year.toString())
+          );
+        });
+      });
+    });
+
+    it('filtering by publication year does not show unpublished posts', () => {
+      const postsWithYear = seededPosts.map((p) => ({
+        ...p,
+        year: new Date(p.publishedAt as Date).getFullYear(),
+      }));
+      const groupedByYear = Cypress._.groupBy(postsWithYear, 'year');
+      const postsOfSameYearThatAreDraftAndPublic = Cypress._.find(
+        groupedByYear,
+        (posts) => {
+          const statuses = Cypress._.map(posts, 'status');
+          return statuses.includes('PUBLISHED') && statuses.includes('DRAFT');
+        }
+      );
+
+      if (!postsOfSameYearThatAreDraftAndPublic)
+        throw new Error('Missing test fixture');
+
+      cy.selectPostPublicationYearFilter(
+        postsOfSameYearThatAreDraftAndPublic[0].year
+      );
+
+      const expectedTitles = postsOfSameYearThatAreDraftAndPublic
+        .filter((p) => p.status === 'PUBLISHED')
+        .map((p) => p.title);
+
+      const unexpectedTitles = postsOfSameYearThatAreDraftAndPublic
+        .filter((p) => p.status !== 'PUBLISHED')
+        .map((p) => p.title);
+
+      cy.get('[aria-label="blog posts"] article h2').each(($h2) => {
+        expect($h2.text()).to.be.oneOf(expectedTitles);
+        expect($h2.text()).not.to.be.oneOf(unexpectedTitles);
+      });
+    });
+  });
 });
