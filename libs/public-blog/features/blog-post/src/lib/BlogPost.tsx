79d12778ac39dc773664fafceeee4f9032039457
Daniel Velkov <danielvelkov13@gmail.com>
2026-02-26 18:00:10 +0200

    feat(blog): add post content as HTML on BlogPost component
    HTML is sanitized using dompurify npm package

--- libs/public-blog/features/blog-post/src/lib/BlogPost.tsx
+++ libs/public-blog/features/blog-post/src/lib/BlogPost.tsx
@@ -1,8 +1,10 @@
 import { useFetchPost } from '@dans-coding-world/public-blog-shared-hooks';
+import DOMPurify from 'dompurify';
 
 export function BlogPost({ postId }: { postId: number }) {
   const { data, isPending, isError, error } = useFetchPost(postId);
   const showLoading = isPending || !data;
+
   return (
     <main>
       {/* Dynamic Content Area */}
@@ -18,6 +20,12 @@ export function BlogPost({ postId }: { postId: number }) {
       ) : (
         <article>
           <h1>{data.post.title}</h1>
+          <div
+            data-testid="post-content"
+            dangerouslySetInnerHTML={{
+              __html: DOMPurify.sanitize(data.post.content),
+            }}
+          ></div>
         </article>
       )}
     </main>
