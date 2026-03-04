c5f2f5599a29fd9736d0ee95e9d8af4cf28a7095
Daniel Velkov <danielvelkov13@gmail.com>
2026-03-04 13:43:17 +0200

    feat(helper): add helper for formatting to relative date
    *build: add moment.js package

--- libs/shared/util/helpers/src/lib/date.helper.ts
+++ libs/shared/util/helpers/src/lib/date.helper.ts
@@ -1,3 +1,5 @@
+import moment from 'moment';
+
 export function formatDateTo_DD_MMM_YYYY(date: Date) {
   return new Intl.DateTimeFormat('en-GB', {
     day: '2-digit',
@@ -20,4 +22,8 @@ export function formatDateTo_Month_DD_YYYY(date: Date) {
     month: 'long',
     day: '2-digit',
   }).format(date);
+}
+
+export function formatToRelativeTimeFromNow(date: Date) {
+  return moment(date).fromNow();
 }
