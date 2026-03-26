078b38375d649937a1b7e998dc80570615e0a854
Daniel Velkov <danielvelkov13@gmail.com>
2026-03-26 17:34:12 +0200

    feat(shared): add method for checking if error response contains details

--- libs/shared/util/api-types/src/index.ts
+++ libs/shared/util/api-types/src/index.ts
@@ -3,3 +3,4 @@ export * from './lib/response-error-details.js';
 export * from './lib/wrappers/collection.js';
 export * from './lib/wrappers/paginated.js';
 export * from './lib/util/is-error-response.js';
+export * from './lib/util/is-error-details-response.js';
