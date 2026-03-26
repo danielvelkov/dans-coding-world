e8e16f2aa82805567498cdf9a62c4143223c2bf7
Daniel Velkov <danielvelkov13@gmail.com>
2026-03-26 17:52:22 +0200

    feat(blog): add periodic user token refresh to useAuth hook

--- libs/public-blog/shared/hooks/src/lib/users/useAuth.ts
+++ libs/public-blog/shared/hooks/src/lib/users/useAuth.ts
@@ -1,11 +1,12 @@
 import { User } from '@dans-coding-world/prisma-schema';
-import { useMutation } from '@tanstack/react-query';
-import { useState } from 'react';
+import { noop, useMutation } from '@tanstack/react-query';
+import { useRef, useState } from 'react';
 import { handleQueryResponse } from '../helper/handle-query-response';
 import { API_ENDPOINTS } from '@dans-coding-world/shared-data-access-api';
 import { api } from '@dans-coding-world/public-blog-data-access-api';
 import { BaseResponse } from '@dans-coding-world/api-types';
 import { LoginDto, LoginResponseDto } from '@dans-coding-world/shared-auth-dto';
+import { TOKEN_CONSTRAINTS } from '@dans-coding-world/shared-constants';
 
 export type UserWithoutPass = Omit<User, 'password'>;
 export type LoginResponseWithoutTokens = Omit<
@@ -14,17 +15,44 @@ export type LoginResponseWithoutTokens = Omit<
 >;
 export function useAuth() {
   const [user, setUser] = useState<UserWithoutPass | null>(null);
+  const refreshIntervalRef = useRef<NodeJS.Timeout | null>(null);
+  const refreshMutateRef = useRef<() => void>(() => noop);
 
-  const loginMutation = useMutation({
-    mutationFn: async (data: LoginDto) => {
+  const refreshMutation = useMutation({
+    mutationFn: async () => {
       const response = await api.post<BaseResponse<LoginResponseWithoutTokens>>(
-        API_ENDPOINTS.AUTH.LOGIN,
-        { data }
+        API_ENDPOINTS.AUTH.REFRESH
       );
       return handleQueryResponse(response);
     },
     onSuccess: (data) => {
       if (data) setUser(data.user);
+    },
+    onError: (error) => {
+      console.log('Could not refresh user session');
+      console.log(error);
+      setUser(null);
+      if (refreshIntervalRef.current) clearInterval(refreshIntervalRef.current);
+    },
+  });
+
+  refreshMutateRef.current = refreshMutation.mutate;
+
+  const loginMutation = useMutation({
+    mutationFn: async (data: LoginDto) => {
+      const response = await api.post<BaseResponse<LoginResponseWithoutTokens>>(
+        API_ENDPOINTS.AUTH.LOGIN,
+        { ...data }
+      );
+      return handleQueryResponse(response);
+    },
+    onSuccess: (data) => {
+      if (data) setUser(data.user);
+      if (refreshIntervalRef.current) clearInterval(refreshIntervalRef.current);
+
+      refreshIntervalRef.current = setInterval(() => {
+        refreshMutateRef.current();
+      }, TOKEN_CONSTRAINTS.ACCESS_TOKEN_EXPIRATION);
     },
   });
 
@@ -35,6 +63,9 @@ export function useAuth() {
     },
     onSuccess: () => {
       setUser(null);
+    },
+    onSettled: () => {
+      if (refreshIntervalRef.current) clearInterval(refreshIntervalRef.current);
     },
   });
 
