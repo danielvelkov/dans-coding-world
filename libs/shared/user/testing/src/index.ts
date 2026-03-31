ba879f3ed3e1651263b674c8512570ea3c78ab1d
Daniel Velkov <danielvelkov13@gmail.com>
2026-03-31 17:21:30 +0300

    feat(shared): generate lib for containing user testing related methods

---
+++ libs/shared/user/testing/src/index.ts
@@ -0,0 +1,2 @@
+export * from './lib/user-response-generator.js';
+export * from './lib/user-generator.js';
---
+++ libs/shared/user/testing/src/lib/user-response-generator.ts
@@ -0,0 +1,22 @@
+import { BaseResponse } from '@dans-coding-world/api-types';
+import { UserDetail } from '@dans-coding-world/user-data-access';
+import { generateRandomUser } from './user-generator.js';
+import { GetUserResponseDto } from '@dans-coding-world/shared-user-dto';
+
+export function generateMockUserResponse({
+  post: user,
+}: {
+  post?: Partial<UserDetail>;
+}): BaseResponse<GetUserResponseDto> {
+  const mockUser = generateRandomUser();
+  return {
+    error: null,
+    success: true,
+    data: {
+      user: {
+        ...mockUser,
+        ...user,
+      },
+    },
+  };
+}
