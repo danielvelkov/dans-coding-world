676528b1786eeb65f6bf60c767c807f91d2f84c4
Daniel Velkov <danielvelkov13@gmail.com>
2026-04-02 12:03:02 +0300

    feat(shared): add fetch user custom hook

---
+++ libs/public-blog/shared/hooks/src/lib/users/useFetchUser.ts
@@ -0,0 +1,25 @@
+import { useQuery } from '@tanstack/react-query';
+import { API_ENDPOINTS } from '@dans-coding-world/shared-data-access-api';
+import { api } from '@dans-coding-world/public-blog-data-access-api';
+import { BaseResponse } from '@dans-coding-world/api-types';
+import { handleQueryResponse } from '../helper/handle-query-response';
+import { GetUserResponseDto } from '@dans-coding-world/shared-user-dto';
+
+export const useFetchUser = (
+  userId: number,
+  options?: { enabled?: boolean }
+) => {
+  const query = useQuery({
+    staleTime: Infinity,
+    queryKey: ['user', userId],
+    queryFn: async () => {
+      const response = await api.get<BaseResponse<GetUserResponseDto>>(
+        API_ENDPOINTS.USERS.BY_ID(userId)
+      );
+      return handleQueryResponse(response);
+    },
+    enabled: options?.enabled ?? !!userId,
+  });
+
+  return query;
+};
