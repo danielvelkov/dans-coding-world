c569d594a93f35a6a266f76eebc2161527ad3807
Daniel Velkov <danielvelkov13@gmail.com>
2026-04-02 15:35:51 +0300

    feat(blog): add UserProfileDropdown component for when user is logged in

---
+++ libs/public-blog/features/header/src/lib/components/UserProfileDropdown.tsx
@@ -0,0 +1,209 @@
+import {
+  UserAvatar,
+  LoadingSpinner,
+} from '@dans-coding-world/public-blog-ui-common';
+import { UserDetail } from '@dans-coding-world/user-data-access';
+import React, { useEffect, useRef, useState } from 'react';
+import { Link } from 'react-router-dom';
+import styled from 'styled-components';
+import { getDisplayName } from '@dans-coding-world/public-blog-shared-helpers';
+
+const StyledButton = styled.button<React.ComponentPropsWithoutRef<'button'>>`
+  background: inherit;
+  border: none;
+  cursor: pointer;
+  color: ${({ theme }) => theme.text.primary};
+`;
+
+const StyledLink = styled(StyledButton)`
+  text-decoration: none;
+  font-size: small;
+`;
+
+const StyledUserProfileDropdown = styled.div<
+  React.ComponentPropsWithRef<'div'>
+>`
+  position: relative;
+
+  > ${StyledButton} {
+    display: flex;
+    align-items: center;
+    gap: 10px;
+    .user-and-email {
+      display: flex;
+      flex-direction: column;
+
+      span:first-child {
+        align-self: center;
+        font-weight: bold;
+        font-size: medium;
+      }
+
+      span:nth-child(2) {
+        align-self: flex-start;
+        color: ${({ theme }) => theme.text.muted};
+        font-size: small;
+      }
+    }
+  }
+`;
+
+const StyledUserAvatar = styled(UserAvatar)`
+  color: ${({ theme }) => theme.text.primary};
+  border: 2px solid ${({ theme }) => theme.border.primary};
+  border-radius: 50%;
+  padding: 6px 8px;
+`;
+
+const StyledLoadingAvatarWrapper = styled.div`
+  position: relative;
+`;
+
+const StyledLoadingSpinner = styled(LoadingSpinner)`
+  position: absolute;
+  height: 100%;
+`;
+
+const StyledCardMenu = styled.ul<React.ComponentPropsWithoutRef<'ul'>>`
+  position: absolute;
+  display: flex;
+  flex-direction: column;
+  z-index: 3;
+  left: 25%;
+  top: 50px;
+  border: 2px solid ${({ theme }) => theme.border.primary};
+  background-color: ${({ theme }) => theme.background.elevated};
+  box-shadow: 0 4px 12px rgba(0, 0, 0, 0.15);
+  gap: 0 !important;
+  align-items: stretch !important;
+  border-radius: 5px;
+
+  ${StyledButton} {
+    border-radius: 3px;
+    display: flex;
+    align-items: baseline;
+    gap: 8px;
+    padding: 10px 12px;
+    text-transform: capitalize;
+    font-weight: 550;
+    font-family: inherit;
+    width: 100%;
+  }
+
+  ${StyledButton}:hover {
+    background-color: ${({ theme }) => theme.accent.primary};
+    color: #fff;
+  }
+
+  .arrow {
+    position: absolute;
+    top: -20px;
+    border-bottom: 8px solid ${({ theme }) => theme.border.primary};
+    border-top: 8px solid transparent;
+
+    border-right: 8px solid transparent;
+    border-left: 8px solid transparent;
+    width: 0;
+    height: 0;
+    align-self: center;
+  }
+`;
+
+export function UserProfileDropdown({
+  user,
+  logoutAction,
+  isLoading,
+}: {
+  user: Omit<UserDetail, 'password'>;
+  logoutAction: () => void;
+  isLoading?: boolean;
+}) {
+  const [isOpen, setIsOpen] = useState(false);
+  const cardMenuRef = useRef<HTMLDivElement>(null);
+
+  useEffect(() => {
+    function handleClickOutside(e: MouseEvent) {
+      if (
+        cardMenuRef.current &&
+        !cardMenuRef.current.contains(e.target as Node)
+      ) {
+        setIsOpen(false);
+      }
+    }
+    document.addEventListener('click', handleClickOutside);
+    return () => document.removeEventListener('click', handleClickOutside);
+  }, []);
+
+  const name = getDisplayName(user);
+
+  return (
+    <StyledUserProfileDropdown ref={cardMenuRef}>
+      <StyledButton
+        aria-haspopup="true"
+        aria-controls="expandable-menu"
+        aria-expanded={isOpen}
+        onClick={() => {
+          setIsOpen((open) => !open);
+        }}
+      >
+        <div className="user-and-email">
+          <span>{name}</span>
+          <span>{user.email}</span>
+        </div>
+        <StyledLoadingAvatarWrapper>
+          {isLoading && <StyledLoadingSpinner />}
+          <StyledUserAvatar
+            name={user.username}
+            size="S"
+            avatarURL={user.profile?.avatarURL}
+          />
+        </StyledLoadingAvatarWrapper>
+      </StyledButton>
+
+      {isOpen && (
+        <StyledCardMenu
+          id="expandable-menu"
+          role="menu"
+          onClick={() => setIsOpen(false)}
+        >
+          <div className="arrow"></div>
+
+          <li role="presentation">
+            <StyledLink to={`/users/${user.id}`} role="menuitem" as={Link}>
+              <i className="fa fa-thin fa-id-card"></i>
+              Profile
+            </StyledLink>
+          </li>
+
+          <li role="presentation">
+            <StyledLink
+              to={`/users/${user.id}/edit`}
+              role="menuitem"
+              as={Link}
+              state={{ mode: 'edit' }}
+            >
+              <i className="fa fa-pen"></i>
+              {user.profile ? 'Edit profile' : 'Setup profile'}
+            </StyledLink>
+          </li>
+
+          <li role="presentation">
+            <StyledLink to={`/settings`} role="menuitem" as={Link}>
+              <i className="fa fa-gear"></i>
+              Settings
+            </StyledLink>
+          </li>
+
+          <li role="presentation">
+            <StyledButton onClick={() => logoutAction()} role="menuitem">
+              <i className="fa fa-sign-out"></i>
+              Logout
+            </StyledButton>
+          </li>
+        </StyledCardMenu>
+      )}
+    </StyledUserProfileDropdown>
+  );
+}
+
+export default UserProfileDropdown;
