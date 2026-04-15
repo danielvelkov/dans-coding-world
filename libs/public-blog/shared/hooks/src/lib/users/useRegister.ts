aa844bbc938bf161fbe0c4ec4e304d6dd86eb624
Daniel Velkov <danielvelkov13@gmail.com>
2026-04-15 13:20:21 +0300

    feat(shared): add user registration custom hook

---
+++ libs/public-blog/shared/hooks/src/lib/users/useRegister.ts
@@ -0,0 +1,31 @@
+import { BaseResponse } from '@dans-coding-world/api-types';
+import { api } from '@dans-coding-world/public-blog-data-access-api';
+import { RegisterDto } from '@dans-coding-world/shared-auth-dto';
+import { useMutation } from '@tanstack/react-query';
+import { UserWithoutPass } from './useAuthState';
+import { API_ENDPOINTS } from '@dans-coding-world/shared-data-access-api';
+import { handleQueryResponse } from '../helper/handle-query-response';
+import useAuth from './useAuth';
+
+export function useRegister() {
+  const { login } = useAuth();
+
+  const registerMutation = useMutation({
+    mutationFn: async (data: RegisterDto) => {
+      const response = await api.post<BaseResponse<{ user: UserWithoutPass }>>(
+        API_ENDPOINTS.AUTH.REGISTER,
+        { ...data }
+      );
+      return handleQueryResponse(response);
+    },
+    onSuccess: (_, variables) => {
+      login({ email: variables.email, password: variables.password });
+    },
+  });
+
+  return {
+    register: registerMutation.mutate,
+    isSubmitting: registerMutation.isPending,
+    error: registerMutation.error,
+  };
+}
