c6db96c07a96e31f713887ceb0fd2a43eb654e56
Daniel Velkov <danielvelkov13@gmail.com>
2026-04-24 18:48:24 +0300

    feat(shared): add useUpdateProfile custom hook

---
+++ libs/public-blog/shared/hooks/src/lib/users/useUpdateProfile.ts
@@ -0,0 +1,39 @@
+import { BaseResponse } from '@dans-coding-world/api-types';
+import { api } from '@dans-coding-world/public-blog-data-access-api';
+import { useMutation, useQueryClient } from '@tanstack/react-query';
+import { UserWithoutPass } from './useAuthState';
+import { API_ENDPOINTS } from '@dans-coding-world/shared-data-access-api';
+import { handleQueryResponse } from '../helper/handle-query-response';
+import { UpdateUserDto } from '@dans-coding-world/shared-user-dto';
+
+export const multipartHeaders = { 'Content-Type': 'multipart/form-data' };
+
+export function useUpdateProfile() {
+  const queryClient = useQueryClient();
+  const updateProfileMutation = useMutation({
+    mutationFn: async (
+      data: Omit<UpdateUserDto, 'avatar'> & { avatar?: File }
+    ) => {
+      const response = await api.patch<BaseResponse<{ user: UserWithoutPass }>>(
+        API_ENDPOINTS.USERS.UPDATE,
+        data,
+        {
+          headers: multipartHeaders,
+        }
+      );
+      return handleQueryResponse(response);
+    },
+    onSuccess: (result) => {
+      if (result?.user)
+        queryClient.invalidateQueries({ queryKey: ['user', result.user.id] });
+    },
+  });
+
+  return {
+    updateProfile: updateProfileMutation.mutate,
+    isSubmitting: updateProfileMutation.isPending,
+    error: updateProfileMutation.error,
+    isSuccess: updateProfileMutation.isSuccess,
+    reset: updateProfileMutation.reset,
+  };
+}
