f092260f162fcdad4786b47f8c3e9c16f9bdaa34
Daniel Velkov <danielvelkov13@gmail.com>
2026-05-06 11:38:42 +0300

    feat(shared): add useCreateComment hook

--- libs/public-blog/shared/hooks/src/index.ts
+++ libs/public-blog/shared/hooks/src/index.ts
@@ -3,6 +3,7 @@ export * from './lib/posts/useFetchPost';
 export * from './lib/posts/useFetchPostCommentsInfinite';
 export * from './lib/posts/useFetchTags';
 export * from './lib/posts/useFetchPostsMetadata';
+export * from './lib/posts/useCreateComment';
 export * from './lib/users/useAuth';
 export * from './lib/users/useRegister';
 export * from './lib/users/useUpdateProfile';
