3437d9d25f4d15f4e178335a98fb438e97077d35
Daniel Velkov <danielvelkov13@gmail.com>
2026-05-14 17:56:09 +0300

    feat(blog): add footer with author info
    *refactor(blog): change root layout to have header and footer

--- apps/public-blog/src/app/RootLayout.tsx
+++ apps/public-blog/src/app/RootLayout.tsx
@@ -1,13 +1,66 @@
 import { useAuth } from '@dans-coding-world/public-blog-shared-hooks';
 import GlobalLoadingOverlay from './GlobalLoadingOverlay';
 import { Outlet } from 'react-router-dom';
+import { Header } from '@dans-coding-world/public-blog-features-header';
+import useThemeDetector from '../styles/useThemeDetector';
+import { useState } from 'react';
+import { darkTheme, lightTheme } from '@dans-coding-world/public-blog-ui-theme';
+import styled, { ThemeProvider } from 'styled-components';
+import GlobalStyle from '../styles/global.style';
+
+const Layout = styled.div`
+  height: 100%;
+  display: flex;
+  flex-direction: column;
+  justify-content: space-between;
+`;
+
+const StyledFooter = styled.footer`
+  padding: 2em 1em;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  gap: 10px;
+
+  hr {
+    width: 30ch;
+    color: ${({ theme }) => theme.border.secondary};
+  }
+`;
 
 function RootLayout() {
   const { isRefreshing } = useAuth();
+  const userDarkMode = useThemeDetector();
+  const [isDarkMode, setIsDarkMode] = useState(userDarkMode);
 
-  if (isRefreshing) return <GlobalLoadingOverlay visible />;
-
-  return <Outlet />;
+  return (
+    <ThemeProvider theme={isDarkMode ? darkTheme : lightTheme}>
+      <GlobalStyle />
+      {isRefreshing ? (
+        <GlobalLoadingOverlay visible />
+      ) : (
+        <Layout>
+          <Header
+            isDarkMode={isDarkMode}
+            setIsDarkMode={setIsDarkMode}
+          ></Header>
+          <Outlet />
+          <StyledFooter>
+            <hr></hr>
+            <span>
+              made by{' '}
+              <a href="https://dv-project-portfolio.netlify.app/#contact">me</a>
+            </span>
+            <div>
+              <a href="https://github.com/danielvelkov">
+                <i className="fa fa-github"></i>
+              </a>
+            </div>
+          </StyledFooter>
+        </Layout>
+      )}
+    </ThemeProvider>
+  );
 }
 
 export default RootLayout;
--- apps/public-blog/src/app/app.tsx
+++ apps/public-blog/src/app/app.tsx
@@ -1,4 +1,4 @@
-import styled, { ThemeProvider } from 'styled-components';
+import styled from 'styled-components';
 
 import { Navigate, Route, Routes } from 'react-router-dom';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
@@ -6,82 +6,65 @@ import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
 import Blog from '../routes/blog/Blog';
 import Post from '../routes/blog/Post';
 import Login from '../routes/user/Login';
-import { useState } from 'react';
-import { darkTheme, lightTheme } from '@dans-coding-world/public-blog-ui-theme';
 import { NotFound } from '@dans-coding-world/public-blog-ui-errors';
-import { GlobalStyle } from '../styles/global.style.js';
 import { ErrorBoundary } from 'react-error-boundary';
 import { Fallback } from './Fallback';
 import { AuthProvider } from '@dans-coding-world/public-blog-shared-hooks';
-import { Header } from '@dans-coding-world/public-blog-features-header';
 import Register from '../routes/user/Register';
 import Profile from '../routes/user/Profile';
 import RootLayout from './RootLayout';
 import Settings from '../routes/user/Settings';
-import useThemeDetector from '../styles/useThemeDetector';
 
 const StyledApp = styled.div`
   padding: 0 clamp(5vmin, 5vw, 15vmax);
   margin: 0 auto;
   max-width: 1320px;
+  height: 100vh;
 `;
 
 // dev tools do not work during e2e testing for some reason
 // also cant set node env in e2e cypress tests
-const showDevTools =
-  process.env.NODE_ENV !== 'development' &&
-  process.env.NODE_ENV !== 'production';
+const showDevTools = process.env.NODE_ENV === 'development';
 
 const queryClient = new QueryClient();
 
 export function App() {
-  const userDarkMode = useThemeDetector();
-  const [isDarkMode, setIsDarkMode] = useState(userDarkMode);
-
   return (
     <QueryClientProvider client={queryClient}>
       {showDevTools && <ReactQueryDevtools initialIsOpen={false} />}
       <AuthProvider>
-        <ThemeProvider theme={isDarkMode ? darkTheme : lightTheme}>
-          <GlobalStyle />
-          <StyledApp>
-            <Header
-              isDarkMode={isDarkMode}
-              setIsDarkMode={setIsDarkMode}
-            ></Header>
-            <Routes>
-              <Route element={<RootLayout />}>
-                {/* TODO add homepage */}
-                <Route index element={<Navigate to="blog" replace />} />
-                <Route path="login" element={<Login />} />
-                <Route path="register" element={<Register />} />
-                <Route path="blog">
-                  <Route index element={<Blog />} />
-                  <Route
-                    path=":postId"
-                    element={
-                      <ErrorBoundary FallbackComponent={Fallback}>
-                        <Post />
-                      </ErrorBoundary>
-                    }
-                  />
-                </Route>
-                <Route path="users">
-                  <Route
-                    path=":userId/*"
-                    element={
-                      <ErrorBoundary FallbackComponent={Fallback}>
-                        <Profile />
-                      </ErrorBoundary>
-                    }
-                  />
-                </Route>
-                <Route path="settings" element={<Settings />} />
-                <Route path="*" element={<NotFound />} />
+        <StyledApp>
+          <Routes>
+            <Route element={<RootLayout />}>
+              <Route index element={<Navigate to="blog" replace />} />
+              <Route path="login" element={<Login />} />
+              <Route path="register" element={<Register />} />
+              <Route path="blog">
+                <Route index element={<Blog />} />
+                <Route
+                  path=":postId"
+                  element={
+                    <ErrorBoundary FallbackComponent={Fallback}>
+                      <Post />
+                    </ErrorBoundary>
+                  }
+                />
               </Route>
-            </Routes>
-          </StyledApp>
-        </ThemeProvider>
+              <Route path="users">
+                <Route
+                  path=":userId/*"
+                  element={
+                    <ErrorBoundary FallbackComponent={Fallback}>
+                      <Profile />
+                    </ErrorBoundary>
+                  }
+                />
+              </Route>
+              <Route path="settings" element={<Settings />} />
+              <Route path="*" element={<NotFound />} />
+            </Route>
+          </Routes>
+        </StyledApp>
       </AuthProvider>
     </QueryClientProvider>
   );
