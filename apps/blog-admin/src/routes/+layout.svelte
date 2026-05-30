766b4d8b678950ef372f668fe74cffcf6e4c20b7
Daniel Velkov <danielvelkov13@gmail.com>
2026-05-30 19:01:35 +0300

    feat(admin): add Tanstack query provider to layout

--- apps/blog-admin/src/routes/+layout.svelte
+++ apps/blog-admin/src/routes/+layout.svelte
@@ -1,11 +1,23 @@
 <script lang="ts">
 	import './layout.css';
 	import favicon from '$lib/assets/favicon.svg';
+	import { browser } from '$app/environment';
+	import { QueryClient, QueryClientProvider } from '@tanstack/svelte-query';
+
+	const queryClient = new QueryClient({
+		defaultOptions: {
+			queries: {
+				enabled: browser
+			}
+		}
+	});
 
 	let { children } = $props();
 </script>
 
 <svelte:head><link rel="icon" href={favicon} /></svelte:head>
-<main class="m-auto flex max-w-2xl flex-col">
-	{@render children()}
-</main>
+<QueryClientProvider client={queryClient}>
+	<main class="m-auto flex max-w-2xl flex-col">
+		{@render children()}
+	</main>
+</QueryClientProvider>
