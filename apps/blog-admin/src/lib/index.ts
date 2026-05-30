bf1c8a2dee1a2b96416106981fc805a81ba93cc1
Daniel Velkov <danielvelkov13@gmail.com>
2026-05-30 19:02:51 +0300

    feat(admin): use svelte query for fetching posts
    Now posts route uses svelte query

--- apps/blog-admin/src/lib/index.ts
+++ apps/blog-admin/src/lib/index.ts
@@ -1,2 +1,1 @@
 // place files you want to import through the `$lib` alias in this folder.
-export { PostsTable } from '@dans-coding-world/blog-admin-features-posts-table';
--- apps/blog-admin/src/routes/posts/+page.svelte
+++ apps/blog-admin/src/routes/posts/+page.svelte
@@ -1,14 +1,34 @@
 <script lang="ts">
-	import { generateMockPostsResponse } from '@dans-coding-world/shared-post-testing';
-	import { PAGINATION } from '@dans-coding-world/shared-constants';
-	import { PostsTable } from '$lib';
-	const { data } = generateMockPostsResponse({
-		length: PAGINATION.POSTS.DEFAULT_ITEMS_PER_PAGE,
-		pageSize: PAGINATION.POSTS.DEFAULT_ITEMS_PER_PAGE
-	});
-	const posts = data?.items;
-	if (!posts) throw new Error('Missing fixtures');
+	import type { GetPostsResponseDto } from '@dans-coding-world/shared-post-dto';
+	import { api } from '@dans-coding-world/public-blog-data-access-api';
+	import type { BaseResponse } from '@dans-coding-world/api-types';
+	import { PostsTable } from '@dans-coding-world/blog-admin-features-posts-table';
+	import { API_ENDPOINTS, handleQueryResponse } from '@dans-coding-world/shared-data-access-api';
+	import { createQuery } from '@tanstack/svelte-query';
+
+	const { data } = $props();
+
+	const TEN_MINUTES_IN_MS = 10 * 60 * 1000;
+	const postsQuery = createQuery<GetPostsResponseDto | null, Error>(() => ({
+		staleTime: TEN_MINUTES_IN_MS,
+		queryKey: ['posts'],
+		queryFn: async () => {
+			const response = await api.get<BaseResponse<GetPostsResponseDto>>(API_ENDPOINTS.POSTS.LIST);
+			return handleQueryResponse(response);
+		},
+		initialData: data.postsResponse
+	}));
 </script>
 
+<svelte:head>
+	<title>Your Posts</title>
+</svelte:head>
+
 <h2 class="mb-2 text-4xl">Posts</h2>
-<PostsTable {posts}></PostsTable>
+{#if postsQuery.status === 'pending'}
+	<span>Loading...</span>
+{:else if postsQuery.status === 'error'}
+	<span>Error: {postsQuery.error.message}</span>
+{:else}
+	<PostsTable posts={postsQuery.data?.items}></PostsTable>
+{/if}
