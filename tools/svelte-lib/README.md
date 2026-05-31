0a985aa9e1a2c4f0fc32d4c9d51a5d39335a2305
Daniel Velkov <danielvelkov13@gmail.com>
2026-05-31 23:05:02 +0300

    build(nx)!: simplify workspace glob pattern in project.json
    Previously each generated lib was added on its own
    This glob pattern handles nested libs
    
    fix(nx): correct generator to not make project.json
    
    Nx devkit method addProjectConfiguration added package.json and project.json files
    Which were not needed;
    also a package.json was already defined as a template in ./files folder of the generator

--- tools/svelte-lib/README.md
+++ tools/svelte-lib/README.md
@@ -5,3 +5,9 @@ This library was generated with [Nx](https://nx.dev).
 ## Building
 
 Run `nx build svelte-lib` to build the library.
+
+## Generators
+
+### Svelte-lib
+
+Run `nx g @dans-coding-world/svelte-lib:sveltelib [name] [directory]`
--- tools/svelte-lib/src/generators/sveltelib/sveltelib.ts
+++ tools/svelte-lib/src/generators/sveltelib/sveltelib.ts
@@ -1,5 +1,4 @@
 import {
-  addProjectConfiguration,
   formatFiles,
   generateFiles,
   joinPathFragments,
@@ -16,27 +15,19 @@ export async function sveltelibGenerator(
 ) {
   const normalized = names(options.name);
 
-  const projectDirectory = options.directory
-    ? `${options.directory}/${normalized.fileName}`
-    : normalized.fileName;
+  const projectDirectory = options.directory;
 
   const projectRoot = joinPathFragments('libs', projectDirectory);
 
-  addProjectConfiguration(tree, normalized.fileName, {
-    root: projectRoot,
-    projectType: 'library',
-    sourceRoot: `${projectRoot}/src`,
-    targets: {},
-  });
+  // Basically the ../../.. path route for the tsconfig.base.json or other related files
+  const relativeToRoot = offsetFromRoot(projectRoot);
 
   // Dynamic cacheDir for Vite
   const cacheDir = joinPathFragments(
+    relativeToRoot,
     'node_modules/.vite',
-    projectDirectory.replace(/\//g, '-'),
+    projectDirectory,
   );
-
-  // Basically the ../../.. path route for the tsconfig.base.json or other related files
-  const relativeToRoot = offsetFromRoot(projectRoot);
 
   generateFiles(tree, path.join(__dirname, 'files'), projectRoot, {
     ...options,
