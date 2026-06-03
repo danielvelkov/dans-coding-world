839b57c2673bdb57e3b95ea74f3619bed1234c9a
Daniel Velkov <danielvelkov13@gmail.com>
2026-06-03 21:33:06 +0300

    feat(admin): add generic table component
    Component takes header and row snippet props.
    This is to make row and header rendering dynamic, but the overall table static

--- libs/blog-admin/ui/common/src/index.ts
+++ libs/blog-admin/ui/common/src/index.ts
@@ -2,3 +2,4 @@
 
 // Example export of component. Notice the "default as", svelte components must be the default export
 export { default as Button } from './lib/Button.svelte';
+export { default as Table } from './lib/Table.svelte';
---
+++ libs/blog-admin/ui/common/src/lib/Table.svelte
@@ -0,0 +1,33 @@
+<script lang="ts" generics="T">
+  import type { Snippet } from 'svelte';
+
+  let {
+    data,
+    header,
+    row,
+    children,
+  }: {
+    data: T[];
+    header: Snippet;
+    row: Snippet<[T]>;
+    children: Snippet<[]>;
+  } = $props();
+</script>
+
+<table class="w-full table-auto text-left border border-gray-300">
+  {#if header}
+    <thead class="border-b border-gray-200 bg-gray-50 text-gray-700">
+      <tr>
+        {@render header()}
+      </tr>
+    </thead>
+  {/if}
+
+  {@render children()}
+
+  <tbody class="divide-y divide-gray-200">
+    {#each data as d (d)}
+      {@render row(d)}
+    {/each}
+  </tbody>
+</table>
