81f3577ee42a1871bcc171a4fdd3cc90d897e4d6
Daniel Velkov <danielvelkov13@gmail.com>
2026-06-03 22:06:31 +0300

    feat(admin): use generic table component in PostsTable

--- libs/blog-admin/features/posts-table/src/lib/PostsTable.svelte
+++ libs/blog-admin/features/posts-table/src/lib/PostsTable.svelte
@@ -3,128 +3,141 @@
   import type { PostFull } from '@dans-coding-world/post-data-access';
   import { formatDateTo_DD_MM_YYYY } from '@dans-coding-world/helpers';
   import type { UserDetail } from '@dans-coding-world/user-data-access';
+  import { Table } from '@dans-coding-world/blog-admin-ui-common';
 
-  export interface Props {
+  interface Props {
     posts?: PostFull[];
     isLoading?: boolean;
     error?: Error;
     viewer?: UserDetail;
   }
-  const {
-    posts = [],
-    isLoading = false,
-    error,
-    viewer,
-  }: Props = $props<Props>();
+
+  interface RowDetails {
+    post: PostFull;
+    rowIndex: number;
+  }
+
+  const { posts = [], isLoading = false, error, viewer }: Props = $props();
 
   const showEmptyMessage = $derived(posts.length === 0);
-
   const isAdmin = $derived(viewer?.role === 'ADMIN');
 </script>
 
-<table class="w-full table-auto text-left border border-gray-300">
-  <thead class="border-b border-gray-200 bg-gray-50 text-gray-700">
-    <tr>
-      {#each TABLE_COLUMNS as col}
-        {@const isAdminCol = col === ADMIN_ONLY_COLUMN}
-        {#if !isAdminCol || (isAdminCol && isAdmin)}
-          <th
-            class={`px-4 py-3 text-sm font-semibold ${['#', 'Published/Edited Date'].includes(col) ? 'text-center' : ''}`}
-            >{col}</th
-          >
+<Table data={posts.map((post, rowIndex) => ({ post, rowIndex }) as RowDetails)}>
+  <!-- Table columns -->
+  {#snippet header()}
+    {#each TABLE_COLUMNS as col}
+      {@render ColumnHeader(col)}
+    {/each}
+  {/snippet}
+
+  <!-- children() -->
+  {#if isLoading}
+    {@render MessageRow('Loading...', 'text-gray-500 italic')}
+  {:else if error}
+    {@render MessageRow(error.message, 'text-red-500 italic')}
+  {:else if showEmptyMessage}
+    {@render MessageRow(
+      'No posts yet - Create your first post',
+      'text-gray-500 italic',
+    )}
+  {/if}
+
+  <!-- Table Rows -->
+  {#snippet row({ post, rowIndex })}
+    {@render PostRow(post, rowIndex)}
+  {/snippet}
+</Table>
+
+{#snippet ColumnHeader(col: string)}
+  {@const isAdminCol = col === ADMIN_ONLY_COLUMN}
+
+  {#if !isAdminCol || isAdmin}
+    <th
+      class={`px-4 py-3 text-sm font-semibold ${
+        ['#', 'Published/Edited Date'].includes(col) ? 'text-center' : ''
+      }`}
+    >
+      {col}
+    </th>
+  {/if}
+{/snippet}
+
+{#snippet MessageRow(message: string, classOverride: string)}
+  <tr>
+    <td
+      colspan={TABLE_COLUMNS.length}
+      class={`px-4 py-6 text-center ${classOverride}`}
+    >
+      {message}
+    </td>
+  </tr>
+{/snippet}
+
+{#snippet PostRow(post: PostFull, rowIndex: number)}
+  {@const isRecentlyUpdated =
+    new Date(post.createdAt).getTime() !== new Date(post.updatedAt).getTime()}
+
+  <tr class="transition-colors hover:bg-gray-50">
+    <!-- Index -->
+    <td class="px-4 py-3 text-sm text-gray-700 text-center">{rowIndex + 1}</td>
+
+    <!-- Title -->
+    <td
+      class="px-4 pt-3 text-sm line-clamp-2 text-ellipsis font-medium text-gray-900"
+      title={post.title}
+    >
+      {post.title}
+    </td>
+
+    <!-- Status/Visibility -->
+    <td class="px-4 py-3 text-sm text-gray-700">
+      <div class="flex flex-col items-left gap-1">
+        <b class="text-gray-900">{post.status.toUpperCase()}</b>
+
+        {#if post.visibility === 'MEMBERS_ONLY'}
+          <span class="text-xs text-gray-500">(Members-only)</span>
         {/if}
-      {/each}
-    </tr>
-  </thead>
+      </div>
+    </td>
 
-  <tbody class="divide-y divide-gray-200">
-    {#if isLoading}
-      <tr>
-        <td
-          colspan={TABLE_COLUMNS.length}
-          class="px-4 py-6 text-center text-gray-500 italic"
-        >
-          Loading...
-        </td>
-      </tr>
-    {:else if error}
-      <tr>
-        <td
-          colspan={TABLE_COLUMNS.length}
-          class="px-4 py-6 text-center text-red-500 italic"
-        >
-          {error}
-        </td>
-      </tr>
-    {:else if showEmptyMessage}
-      <tr>
-        <td
-          colspan={TABLE_COLUMNS.length}
-          class="px-4 py-6 text-center text-gray-500 italic"
-        >
-          No posts yet - Create your first post
-        </td>
-      </tr>
-    {:else}
-      {#each posts as post, i}
-        {@const isRecentlyUpdated =
-          new Date(post.createdAt).getTime() !==
-          new Date(post.updatedAt).getTime()}
-
-        <tr class="transition-colors hover:bg-gray-50">
-          <!-- Index col -->
-          <td class="px-4 py-3 text-sm text-gray-700 text-center">{i + 1}</td>
-
-          <!-- Title col -->
-          <td
-            class="px-4 pt-3 text-sm line-clamp-2 text-ellipsis font-medium text-gray-900"
-            title={post.title}
+    <!-- Published/Updated -->
+    <td class="px-4 py-3 text-sm text-gray-700">
+      <div class="flex flex-col items-center gap-1">
+        {#if post.publishedAt}
+          <time
+            datetime={new Date(post.publishedAt).toISOString()}
+            class="text-gray-900"
           >
-            {post.title}
-          </td>
+            {formatDateTo_DD_MM_YYYY(new Date(post.publishedAt))}
+          </time>
+        {:else}
+          <span class="text-gray-400">-</span>
+        {/if}
 
-          <!-- Status/Visibility col -->
-          <td class="px-4 py-3 text-sm text-gray-700">
-            <div class="flex flex-col items-left gap-1">
-              <b class="text-gray-900">{post.status.toUpperCase()}</b>
+        {#if isRecentlyUpdated}
+          <span class="text-xs text-gray-500">
+            {formatDateTo_DD_MM_YYYY(new Date(post.updatedAt))}
+          </span>
+        {/if}
+      </div>
+    </td>
 
-              {#if post.visibility === 'MEMBERS_ONLY'}
-                <span class="text-xs text-gray-500">(Members-only)</span>
-              {/if}
-            </div>
-          </td>
-
-          <!-- Published/Updated date col -->
-          <td class="px-4 py-3 text-sm text-gray-700">
-            <div class="flex flex-col items-center gap-1">
-              {#if post.publishedAt}
-                <time
-                  datetime={new Date(post.publishedAt).toISOString()}
-                  class="text-gray-900"
-                >
-                  {formatDateTo_DD_MM_YYYY(new Date(post.publishedAt))}
-                </time>
-              {:else}
-                <span class="text-gray-400">-</span>
-              {/if}
-              {#if isRecentlyUpdated}
-                <span class="text-xs text-gray-500"
-                  >{formatDateTo_DD_MM_YYYY(new Date(post.updatedAt))}</span
-                >
-              {/if}
-            </div>
-          </td>
-
-          <!-- Author col (admin-only) -->
-          {#if isAdmin}
-            <td class="px-4 py-3 text-sm text-gray-700">
-              <a href={`/users?search=${post.authorId}`}>
-                <b class="text-gray-900">{post.author.username}</b>
-              </a>
-            </td>
-          {/if}
-        </tr>
-      {/each}
+    <!-- Author (admin-only) -->
+    {#if isAdmin}
+      <td class="px-4 py-3 text-sm text-gray-700">
+        <a href={`/users?search=${post.authorId}`}>
+          <b class="text-gray-900 hover:underline">{post.author.username}</b>
+        </a>
+      </td>
     {/if}
-  </tbody>
-</table>
+
+    <!-- Actions -->
+    <td class="px-4 py-3 text-sm text-gray-700">
+      <div class="flex flex-wrap gap-3">
+        <a href={`/posts/${post.id}/edit`}>Edit</a>
+        <button>Delete</button>
+      </div>
+    </td>
+  </tr>
+{/snippet}
