50959083ed703387c3c9f5bcc690aa7f5a5337d6
Daniel Velkov <danielvelkov13@gmail.com>
2026-06-15 21:31:07 +0300

    feat(util): export seed options type from api tools lib

--- libs/tools/api/src/lib/seeders/users.seeder.ts
+++ libs/tools/api/src/lib/seeders/users.seeder.ts
@@ -1,4 +1,4 @@
-import users from '../data/users.json' with {type: "json"};
+import users from '../data/users.json' with { type: 'json' };
 import { client, Role, User } from '@dans-coding-world/prisma-schema';
 import { hashPassword } from '@dans-coding-world/helpers';
 import { SeedOptions } from './types/seed-options.js';
@@ -10,7 +10,7 @@ import { SeedOptions } from './types/seed-options.js';
  *
  * @param customUsers Users to create.
  * @param options Seed options for whether to clear and reset the 'User' table
- * 
+ *
  * *DEFAULT DATA*:
  * - Id: 1 - **Admin**
  * - Id: 2 - **Mod**
@@ -19,32 +19,35 @@ import { SeedOptions } from './types/seed-options.js';
  */
 export const seedUsers = async (
   customUsers?: User[],
-  options: SeedOptions = { clearExisting: true, useDefaults: true }
+  options: SeedOptions = { clearExisting: true, useDefaults: true },
 ): Promise<User[]> => {
   if (!(process.env.NODE_ENV === 'test' || process.env.NODE_ENV === 'test_e2e'))
     throw new Error(
-      'Not in test environment. Check your test setup configuration.'
+      'Not in test environment. Check your test setup configuration.',
     );
   try {
     const seeded: User[] = [];
 
     if (options.clearExisting) {
-       await client.$transaction(async (tx) => {
-        await tx.$executeRaw`TRUNCATE TABLE "User" CASCADE`;
-        await tx.$executeRaw`ALTER SEQUENCE "User_id_seq" RESTART WITH 1`;
-        await tx.$executeRaw`ALTER SEQUENCE "Profile_id_seq" RESTART WITH 1`;
-      }, {
-        isolationLevel: 'Serializable'
-      });
+      await client.$transaction(
+        async (tx) => {
+          await tx.$executeRaw`TRUNCATE TABLE "User" CASCADE`;
+          await tx.$executeRaw`ALTER SEQUENCE "User_id_seq" RESTART WITH 1`;
+          await tx.$executeRaw`ALTER SEQUENCE "Profile_id_seq" RESTART WITH 1`;
+        },
+        {
+          isolationLevel: 'Serializable',
+        },
+      );
     }
 
     if (options.useDefaults) {
-      const defaultUsers = await createAndReturnUsersWithId(users)
+      const defaultUsers = await createAndReturnUsersWithId(users);
       seeded.push(...defaultUsers);
     }
 
     if (customUsers && Array.isArray(customUsers)) {
-      const newUsers = await createAndReturnUsersWithId(customUsers)
+      const newUsers = await createAndReturnUsersWithId(customUsers);
       seeded.push(...newUsers);
     }
     return seeded;
@@ -68,15 +71,15 @@ const createAndReturnUsersWithId = async (users: any[]) => {
         email: u.email,
         password,
         role: u.role as Role,
-        isBanned: false
+        isBanned: false,
       };
       if (typeof u.isBanned === 'boolean') data.isBanned = u.isBanned;
       return data;
-    })
+    }),
   );
 
   const createdUsers = await client.$transaction(
-    usersWithHashedPassword.map((user) => client.user.create({ data: user }))
+    usersWithHashedPassword.map((user) => client.user.create({ data: user })),
   );
   return createdUsers.map((u, i) => ({ ...u, password: users[i].password }));
 };
