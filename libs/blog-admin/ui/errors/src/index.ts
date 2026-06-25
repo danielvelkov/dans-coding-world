e114a1477322c5854380d14c60bf523496db8c61
Daniel Velkov <danielvelkov13@gmail.com>
2026-06-25 22:11:24 +0300

    feat(ui): add Unauthorized error svelte ui component

--- libs/blog-admin/ui/errors/src/index.ts
+++ libs/blog-admin/ui/errors/src/index.ts
@@ -3,4 +3,5 @@
 // Example export of component. Notice the "default as", svelte components must be the default export
 export { default as GenericError } from './lib/GenericError.svelte';
 export { default as Forbidden } from './lib/Forbidden.svelte';
+export { default as Unauthorized } from './lib/Unauthorized.svelte';
 export { default as NotFound } from './lib/NotFound.svelte';
