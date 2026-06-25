4f6caaaf69c93bbe5a3efd68972436a81845ddcc
Daniel Velkov <danielvelkov13@gmail.com>
2026-06-25 10:54:19 +0300

    feat(ui): add lib containing error pages Forbidden and NotFound

---
+++ libs/blog-admin/ui/errors/src/lib/GenericError.svelte
@@ -0,0 +1,24 @@
+<script lang="ts">
+  import { StatusCodes, ReasonPhrases } from 'http-status-codes';
+  import type { Snippet } from 'svelte';
+
+  interface Props {
+    statusCode?: number;
+    message?: string;
+    children: Snippet<[]>;
+  }
+
+  const { statusCode, message, children }: Props = $props();
+</script>
+
+<div
+  class="mt-[20vh] flex flex-col items-center justify-center bg-(--color-background)"
+>
+  <h1 class="text-6xl font-bold text-(--color-foreground)">
+    {statusCode ?? StatusCodes.INTERNAL_SERVER_ERROR}
+  </h1>
+  <p class="mt-4 text-lg text-(--color-muted-foreground)">
+    {message ?? ReasonPhrases.INTERNAL_SERVER_ERROR}
+  </p>
+  {@render children()}
+</div>
---
+++ libs/blog-admin/ui/errors/src/lib/NotFound.svelte
@@ -0,0 +1,18 @@
+<script lang="ts">
+  import { StatusCodes, ReasonPhrases } from 'http-status-codes';
+  import type { Snippet } from 'svelte';
+  import GenericError from './GenericError.svelte';
+
+  interface Props {
+    message?: string;
+    children: Snippet<[]>;
+  }
+
+  const { message, children }: Props = $props();
+</script>
+
+<GenericError
+  statusCode={StatusCodes.NOT_FOUND}
+  message={message ?? ReasonPhrases.NOT_FOUND}
+  {children}
+></GenericError>
