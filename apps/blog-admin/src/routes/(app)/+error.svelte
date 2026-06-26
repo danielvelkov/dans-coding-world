2169d186bd6ec51ff468ca4d46ec08e8099a8ba2
Daniel Velkov <danielvelkov13@gmail.com>
2026-06-26 16:03:41 +0300

    feat(admin): setup svelte experimental render error handling
    Now errors happening in script tags are handled by the +error.svelte route

---
+++ apps/blog-admin/src/routes/(app)/+error.svelte
@@ -0,0 +1,9 @@
+<script lang="ts">
+	import { page } from '$app/state';
+	import { GenericError } from '@dans-coding-world/blog-admin-ui-errors';
+</script>
+
+<div class="flex flex-col items-center justify-center bg-(--color-background)">
+	<GenericError message={page.error?.message ?? 'Something went wrong'} statusCode={page.status}
+	></GenericError>
+</div>
