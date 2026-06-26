a712eec5bacc573853045ccdc9872dab5ba10f16
Daniel Velkov <danielvelkov13@gmail.com>
2026-06-26 15:57:40 +0300

    refactor(admin): move query client instance into $libs to be used as singleton
    Because the app is SPA, not SSR-  the query client state won't leak

--- apps/blog-admin/src/routes/+layout.svelte
+++ apps/blog-admin/src/routes/+layout.svelte
@@ -1,32 +1,16 @@
 <script lang="ts">
 	import './layout.css';
 	import favicon from '$lib/assets/favicon.svg';
-	import { resolve } from '$app/paths';
-	import { QueryClient, QueryClientProvider } from '@tanstack/svelte-query';
-	import type { Snippet } from 'svelte';
+	import { QueryClientProvider } from '@tanstack/svelte-query';
+	import { queryClient } from '$lib/shared/query-client';
 
-	interface Props {
-		data: {
-			queryClient: QueryClient;
-		};
-		children: Snippet<[]>;
-	}
-
-	const { data, children }: Props = $props();
+	const { children } = $props();
 </script>
 
 <svelte:head><link rel="icon" href={favicon} /></svelte:head>
-<QueryClientProvider client={data.queryClient}>
-	<header class="border-b border-gray-200 bg-white shadow-sm">
-		<nav
-			class="mx-auto flex max-w-7xl items-center gap-6 px-6 py-4 text-sm font-medium text-gray-700"
-		>
-			<a href={resolve('/')} class="transition-colors hover:text-gray-900">Home</a>
-			<a href={resolve('/posts')} class="transition-colors hover:text-gray-900">Posts</a>
-		</nav>
-	</header>
 
-	<main class="m-auto flex max-w-2xl flex-col">
+<QueryClientProvider client={queryClient}>
+	<div class="flex min-h-screen min-w-screen flex-col align-middle">
 		{@render children()}
-	</main>
+	</div>
 </QueryClientProvider>
