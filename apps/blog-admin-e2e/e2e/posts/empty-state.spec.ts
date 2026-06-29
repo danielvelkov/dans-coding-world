aac4b90b9587193125e7bdd93e439495d8783255
Daniel Velkov <danielvelkov13@gmail.com>
2026-06-29 13:00:23 +0300

    test(e2e): add login to the loading and empty state e2e tests of /posts

--- apps/blog-admin-e2e/e2e/posts/empty-state.spec.ts
+++ apps/blog-admin-e2e/e2e/posts/empty-state.spec.ts
@@ -1,10 +1,30 @@
 import { API_ENDPOINTS } from '@dans-coding-world/shared-data-access-api';
 import { generateMockPostsResponse } from '@dans-coding-world/shared-post-testing';
-import { test, expect } from '@playwright/test';
+import { test, expect } from '../fixtures/dbFixture';
+import {
+  checkIfLoggedIn,
+  loginAsRandomUser,
+} from '../helpers/user-login.helper';
+import type { User } from '@dans-coding-world/prisma-schema';
 test.describe('Posts page - empty state', () => {
-  test.beforeAll(async () => {
-    //TODO - login
+  let users: User[] = [];
+  test.beforeAll(async ({ db }) => {
+    users = (await db.seedUsers({
+      users: [],
+      options: { useDefaults: true, clearExisting: true },
+    })) as User[];
+    if (!users?.length) throw new Error('Missing user fixtures');
   });
+
+  test.beforeEach(async ({ page }) => {
+    await page.goto('/');
+    await loginAsRandomUser(
+      page,
+      users.filter((u) => u.role !== 'USER'),
+    );
+    expect(await checkIfLoggedIn(page)).toBe(true);
+  });
+
   test('shows call to action message when no table rows', async ({ page }) => {
     await page.route(`**${API_ENDPOINTS.POSTS.LIST}**`, (route) =>
       route.fulfill({
