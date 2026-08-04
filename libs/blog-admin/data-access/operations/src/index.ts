38c042970188c7665889173a1fd04f7be2e52712
Daniel Velkov <danielvelkov13@gmail.com>
2026-08-04 16:10:27 +0300

    feat(admin): add generator fn for delete query of comments

--- libs/blog-admin/data-access/operations/src/index.ts
+++ libs/blog-admin/data-access/operations/src/index.ts
@@ -12,6 +12,7 @@ export * from './lib/mutations/createPostCreationMutation.js';
 export * from './lib/mutations/createPostEditMutation.js';
 export * from './lib/mutations/createEditReportMutation.js';
 export * from './lib/mutations/createDeleteReportMutation.js';
+export * from './lib/mutations/createDeleteCommentMutation.js';
 export * from './lib/types/fetchPostsQueryParams.js';
 export * from './lib/types/fetchUsersQueryParams.js';
 export * from './lib/util/debounceCallback.js';
